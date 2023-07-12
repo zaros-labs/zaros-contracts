d07cb7c81c978e212d9809710e01d8878d1d6060
pedrobergamini <pedrohgbergamini@gmail.com>
2023-07-12 14:51:18 -0300

    chore: create strategy module

---
+++ src/core/modules/StrategyManagerModule.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.19;
+
+// Zaros dependencies
+import { IStrategyManagerModule } from "../interfaces/IStrategyManagerModule.sol";
+
+contract StrategyManagerModule is IStrategyManagerModule {
+    function registerStrategy(address strategy, address collateralType) external { }
+}
---
+++ src/core/storage/StrategyManager.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.19;
+
+library Strategy {
+    string internal constant _STRATEGY_DOMAIN = "fi.zaros.core.Strategy";
+
+    struct Data {
+        /**
+         * @dev The address of the strategy.
+         */
+        address strategy;
+    }
+
+    function load(address collateralType) internal pure returns (Data storage strategy) {
+        bytes32 s = keccak256(abi.encode(_STRATEGY_DOMAIN, collateralType));
+        assembly {
+            strategy.slot := s
+        }
+    }
+}
