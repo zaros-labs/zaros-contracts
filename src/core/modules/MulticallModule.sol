151288e187cf917789a4de7a3d8b955b2442ac4b
pedrobergamini <pedrohgbergamini@gmail.com>
2023-07-14 14:23:41 -0300

    chore: create multicall lib

--- src/core/modules/MulticallModule.sol
+++ src/core/modules/MulticallModule.sol
@@ -2,20 +2,11 @@
 
 pragma solidity 0.8.19;
 
+// Zaros dependencies
+import { Multicall } from "@zaros/utils/Multicall.sol";
+
 contract MulticallModule {
     function multicall(bytes[] calldata data) external payable returns (bytes[] memory results) {
-        results = new bytes[](data.length);
-        for (uint256 i = 0; i < data.length; i++) {
-            (bool success, bytes memory result) = address(this).delegatecall(data[i]);
-
-            if (!success) {
-                uint256 len = result.length;
-                assembly {
-                    revert(add(result, 0x20), len)
-                }
-            }
-
-            results[i] = result;
-        }
+        return Multicall.execute(data);
     }
 }
