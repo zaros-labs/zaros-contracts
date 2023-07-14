8bf5d38732c2fb0b62adfa97a8b4940bde051bd2
pedrobergamini <pedrohgbergamini@gmail.com>
2023-07-14 18:45:15 -0300

    chore: update deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -8,10 +8,10 @@ import { AccountNFT } from "@zaros/account-nft/AccountNFT.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-    function run() public broadcaster returns (address) {
-        AccountNFT accountNFT = new AccountNFT();
-        Zaros zaros = new Zaros(address(accountNFT));
+    function run() public broadcaster returns (address, address) {
+        AccountNFT accountNft = new AccountNFT();
+        Zaros zaros = new Zaros(address(accountNft));
 
-        return address(zaros);
+        return (address(accountNft), address(zaros));
     }
 }
