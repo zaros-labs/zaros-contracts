60d1a3bbaa7da1997b73d2c49996a7f503b91e69
pedrobergamini <pedrohgbergamini@gmail.com>
2023-07-27 18:57:14 -0300

    test: AccountModuleTest

---
+++ test/unit/core/AccountModuleTest.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.19;
+
+// Forge dependencies
+import { Test } from "forge-std/Test.sol";
+
+contract AccountModuleTest is Test {
+    function setUp() public { }
+}
