f145d8f664078260974d98851bb457197d7efc2c
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2023-09-04 16:48:25 -0300

    chore: implement enumerablemap on perps account

--- src/markets/perpetual-futures/vault/modules/PerpsAccountModule.sol
+++ src/markets/perpetual-futures/vault/modules/PerpsAccountModule.sol
@@ -11,6 +11,7 @@ import { PerpsAccount } from "../storage/PerpsAccount.sol";
 import { SystemPerpsMarketsConfiguration } from "../storage/SystemPerpsMarketsConfiguration.sol";
 
 // Open Zeppelin dependencies
+import { EnumerableMap } from "@openzeppelin/utils/structs/EnumerableMap.sol";
 import { IERC20 } from "@openzeppelin/token/ERC20/ERC20.sol";
 import { SafeERC20 } from "@openzeppelin/token/ERC20/utils/SafeERC20.sol";
 
@@ -18,6 +19,7 @@ import { SafeERC20 } from "@openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import { UD60x18, ud60x18 } from "@prb-math/UD60x18.sol";
 
 contract PerpsAccountModule is IPerpsAccountModule {
+    using EnumerableMap for EnumerableMap.AddressToUintMap;
     using PerpsAccount for PerpsAccount.Data;
     using SafeERC20 for IERC20;
     using SystemPerpsMarketsConfiguration for SystemPerpsMarketsConfiguration.Data;
@@ -32,7 +34,7 @@ contract PerpsAccountModule is IPerpsAccountModule {
     {
         PerpsAccount.Data storage perpsAccount = PerpsAccount.load(accountId);
 
-        return ud60x18(perpsAccount.availableMargin[collateralType]);
+        return ud60x18(perpsAccount.availableMargin.get(collateralType));
     }
 
     function getTotalAvailableMargin(uint256 accountId) external view returns (UD60x18) { }
--- src/markets/perpetual-futures/vault/storage/PerpsAccount.sol
+++ src/markets/perpetual-futures/vault/storage/PerpsAccount.sol
@@ -2,15 +2,20 @@
 
 pragma solidity 0.8.19;
 
+// Open Zeppelin dependencies
+import { EnumerableMap } from "@openzeppelin/utils/structs/EnumerableMap.sol";
+
 // PRB Math dependencies
 import { UD60x18, ud60x18 } from "@prb-math/UD60x18.sol";
 
 library PerpsAccount {
+    using EnumerableMap for EnumerableMap.AddressToUintMap;
+
     string internal constant PERPS_ACCOUNT_DOMAIN = "fi.zaros.markets.PerpsAccount";
 
     struct Data {
         uint256 id;
-        mapping(address collateralType => uint256) availableMargin;
+        EnumerableMap.AddressToUintMap availableMargin;
     }
 
     /// @dev TODO: use account id / nft id instead of address
@@ -22,12 +27,20 @@ library PerpsAccount {
     }
 
     function increaseAvailableMargin(Data storage perpsAccount, address collateralType, UD60x18 amount) internal {
-        perpsAccount.availableMargin[collateralType] =
-            ud60x18(perpsAccount.availableMargin[collateralType]).add(amount).intoUint256();
+        EnumerableMap.AddressToUintMap storage availableMargin = perpsAccount.availableMargin;
+        uint256 newAvailableMargin = ud60x18(availableMargin.get(collateralType)).add(amount).intoUint256();
+
+        availableMargin.set(collateralType, newAvailableMargin);
     }
 
     function decreaseAvailableMargin(Data storage perpsAccount, address collateralType, UD60x18 amount) internal {
-        perpsAccount.availableMargin[collateralType] =
-            ud60x18(perpsAccount.availableMargin[collateralType]).sub(amount).intoUint256();
+        EnumerableMap.AddressToUintMap storage availableMargin = perpsAccount.availableMargin;
+        uint256 newAvailableMargin = ud60x18(availableMargin.get(collateralType)).sub(amount).intoUint256();
+
+        if (newAvailableMargin == 0) {
+            availableMargin.remove(collateralType);
+        } else {
+            availableMargin.set(collateralType, newAvailableMargin);
+        }
     }
 }
