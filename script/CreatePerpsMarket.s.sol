704ed1fa5fa807ae94a0d4d5dbd881f70374f41f
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2023-10-31 11:34:34 -0300

    script: move perps market creation logic to a separate script

--- script/CreatePerpsMarket.s.sol
+++ script/CreatePerpsMarket.s.sol
@@ -7,20 +7,31 @@ import { PerpsEngine } from "@zaros/markets/perps/PerpsEngine.sol";
 import { OrderFees } from "@zaros/markets/perps/storage/OrderFees.sol";
 import { BaseScript } from "./Base.s.sol";
 
-contract DeployAlphaPerps is BaseScript {
+contract CreatePerpsMarket is BaseScript {
     /*//////////////////////////////////////////////////////////////////////////
                                      VARIABLES
     //////////////////////////////////////////////////////////////////////////*/
-    bytes32 internal streamId;
-    address internal priceFeed;
+    bytes32 internal ethUsdStreamId;
+    address internal ethUsdPriceFeed;
 
-    uint128 internal constant MARKET_ID = 2;
-    string internal constant MARKET_NAME = "LINK/USD Perpetual";
-    string internal constant MARKET_SYMBOL = "LINK/USD-PERP";
-    uint128 internal constant MARKET_MMR = 0.01e18;
-    uint128 internal constant MARKET_MAX_OI = 100_000_000e18;
-    uint128 internal constant MARKET_MIN_IMR = 0.01e18;
-    OrderFees.Data public orderFees = OrderFees.Data({ makerFee: 0.04e18, takerFee: 0.08e18 });
+    uint128 internal constant ETH_USD_MARKET_ID = 1;
+    string internal constant ETH_USD_MARKET_NAME = "ETH/USD Perpetual Futures";
+    string internal constant ETH_USD_MARKET_SYMBOL = "ETH/USD PERP";
+    uint128 internal constant ETH_USD_MMR = 0.01e18;
+    uint128 internal constant ETH_USD_MAX_OI = 100_000_000e18;
+    uint128 internal constant ETH_USD_MIN_IMR = 0.01e18;
+    OrderFees.Data internal ethUsdOrderFee = OrderFees.Data({ makerFee: 0.04e18, takerFee: 0.08e18 });
+
+    bytes32 internal linkUsdStreamId;
+    address internal linkUsdPriceFeed;
+
+    uint128 internal constant LINK_USD_MARKET_ID = 2;
+    string internal constant LINK_USD_MARKET_NAME = "LINK/USD Perpetual";
+    string internal constant LINK_USD_MARKET_SYMBOL = "LINK/USD-PERP";
+    uint128 internal constant LINK_USD_MMR = 0.01e18;
+    uint128 internal constant LINK_USD_MAX_OI = 100_000_000e18;
+    uint128 internal constant LINK_USD_MIN_IMR = 0.01e18;
+    OrderFees.Data internal linkUsdOrderFee = OrderFees.Data({ makerFee: 0.04e18, takerFee: 0.08e18 });
 
     /*//////////////////////////////////////////////////////////////////////////
                                     CONTRACTS
@@ -28,21 +39,36 @@ contract DeployAlphaPerps is BaseScript {
     PerpsEngine internal perpsEngine;
 
     function run() public broadcaster {
-        streamId = vm.envBytes32("LINK_USD_STREAM_ID");
-        priceFeed = vm.envAddress("LINK_USD_PRICE_FEED");
+        ethUsdStreamId = vm.envBytes32("ETH_USD_STREAM_ID");
+        ethUsdPriceFeed = vm.envAddress("ETH_USD_PRICE_FEED");
+
+        linkUsdStreamId = vm.envBytes32("LINK_USD_STREAM_ID");
+        linkUsdPriceFeed = vm.envAddress("LINK_USD_PRICE_FEED");
 
         perpsEngine = PerpsEngine(payable(address(vm.envAddress("PERPS_ENGINE"))));
 
         perpsEngine.createPerpsMarket(
-            MARKET_ID,
-            MARKET_NAME,
-            MARKET_SYMBOL,
-            streamId,
-            priceFeed,
-            MARKET_MMR,
-            MARKET_MAX_OI,
-            MARKET_MIN_IMR,
-            orderFees
+            ETH_USD_MARKET_ID,
+            ETH_USD_MARKET_NAME,
+            ETH_USD_MARKET_SYMBOL,
+            ethUsdStreamId,
+            ethUsdPriceFeed,
+            ETH_USD_MMR,
+            ETH_USD_MAX_OI,
+            ETH_USD_MIN_IMR,
+            ethUsdOrderFee
+        );
+
+        perpsEngine.createPerpsMarket(
+            LINK_USD_MARKET_ID,
+            LINK_USD_MARKET_NAME,
+            LINK_USD_MARKET_SYMBOL,
+            linkUsdStreamId,
+            linkUsdPriceFeed,
+            LINK_USD_MMR,
+            LINK_USD_MAX_OI,
+            LINK_USD_MIN_IMR,
+            linkUsdOrderFee
         );
     }
 }
