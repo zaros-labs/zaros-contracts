0c16e539ce439d83462006c65f8e99e78af22c45
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2023-11-03 14:53:58 -0300

    test: getTotalAccountMarginCollateralValue

--- test/integration/perps/perps-account-module/getAccountMarginCollateral/getAccountMarginCollateral.t.sol
+++ test/integration/perps/perps-account-module/getAccountMarginCollateral/getAccountMarginCollateral.t.sol
@@ -10,14 +10,16 @@ contract GetAccountMarginCollateral_Integration_Test is Base_Integration_Shared_
         Base_Integration_Shared_Test.setUp();
     }
 
-    function test_GetAccountMarginCollateral() external {
-        uint256 amount = 100e18;
-        uint256 perpsAccountId = _createAccountAndDeposit(amount, address(usdToken));
+    function testFuzz_GetAccountMarginCollateral(uint256 amountToDeposit) external {
+        vm.assume({ condition: amountToDeposit > 0 });
+        deal({ token: address(usdToken), to: users.naruto, give: amountToDeposit });
+
+        uint256 perpsAccountId = _createAccountAndDeposit(amountToDeposit, address(usdToken));
 
         uint256 marginCollateral = perpsEngine.getAccountMarginCollateral({
             accountId: perpsAccountId,
             collateralType: address(usdToken)
         }).intoUint256();
-        assertEq(marginCollateral, amount, "getAccountMarginCollateral");
+        assertEq(marginCollateral, amountToDeposit, "getAccountMarginCollateral");
     }
 }
