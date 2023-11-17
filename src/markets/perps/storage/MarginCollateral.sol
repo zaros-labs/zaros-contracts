0b5873342b22ba93f02cb56792b59c8b0ad5a6b4
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2023-11-17 17:14:23 -0300

    feat: complete MarginCollateral

--- src/markets/perps/storage/MarginCollateral.sol
+++ src/markets/perps/storage/MarginCollateral.sol
@@ -2,12 +2,19 @@
 
 pragma solidity 0.8.19;
 
+// Zaros dependencies
+import { Constants } from "@zaros/utils/Constants.sol";
+import { IAggregatorV3 } from "@zaros/external/interfaces/chainlink/IAggregatorV3.sol";
+
 // PRB Math dependencies
-import { UD60x18 } from "@prb-math/UD60x18.sol";
+import { UD60x18, ud60x18 } from "@prb-math/UD60x18.sol";
 
 library MarginCollateral {
-    /// @notice Thrown when `collateralType` doesn't have a price feed defined to return its price.
-    error CollateralPriceFeedNotDefined(address collateralType);
+    /// @notice Thrown when the {MarginCollateral} doesn't have a price feed defined to return its price.
+    error CollateralPriceFeedNotDefined();
+
+    /// @notice Thrown when `collateralType` decimals are greater than the system's decimals.
+    error InvalidMarginCollateralDecimals(uint8 decimals);
 
     /// @notice Constant base domain used to access a given MarginCollateral's storage slot.
     string internal constant MARGIN_COLLATERAL_DOMAIN = "fi.zaros.markets.MarginCollateral";
@@ -39,6 +46,18 @@ library MarginCollateral {
         depositCap = ud60x18(self.depositCap);
     }
 
+    /// @notice Converts the provided denormalized amount of margin collateral to the system's decimals.
+    /// @dev We can assume self.decimals is always <= SYSTEM_DECIMALS, since it's a requirement at `setDecimals`.
+    /// @param self The margin collateral type storage pointer.
+    /// @param amount The amount of margin collateral to convert.
+    /// @return systemAmount The converted amount of margin collateral to the system's decimals.
+    function getSystemTokenAmount(Data storage self, uint256 amount) internal view returns (UD60x18) {
+        if (Constants.SYSTEM_DECIMALS == self.decimals) {
+            return ud60x18(amount);
+        }
+        return ud60x18(amount * 10 ** (Constants.SYSTEM_DECIMALS - self.decimals));
+    }
+
     /// @notice Updates the deposit cap of a given collateral type. If zero, it is considered
     /// disabled.
     /// @dev If the collateral is enabled, a price feed must be set.
@@ -46,6 +65,13 @@ library MarginCollateral {
     /// @param depositCap The maximum amount of collateral that can be deposited.
     function setDepositCap(Data storage self, uint248 depositCap) internal {
         self.depositCap = depositCap;
+    }
+
+    function setDecimals(Data storage self, uint8 decimals) internal {
+        if (decimals > Constants.SYSTEM_DECIMALS) {
+            revert InvalidMarginCollateralDecimals(decimals);
+        }
+        self.decimals = decimals;
     }
 
     /// @notice Configures the Chainlink Price Feed address of the given margin collateral type.
@@ -58,20 +84,21 @@ library MarginCollateral {
     /// @notice Returns the price of the given margin collateral type.
     /// @param self The margin collateral type storage pointer.
     /// @return price The price of the given margin collateral type.
-    function getCollateralPrice(Data storage self) internal view returns (UD60x18 price) {
+    function getPrice(Data storage self) internal view returns (UD60x18 price) {
         address priceFeed = self.priceFeed;
         if (priceFeed == address(0)) {
-            revert CollateralPriceFeedNotDefined(collateralType);
+            revert CollateralPriceFeedNotDefined();
         }
 
         price = getPrice(self, IAggregatorV3(priceFeed));
     }
 
-    /// @notice Queries the Chainlink Price Feed for the margin collateral oracle price.
+    /// @notice Queries the provided Chainlink Price Feed for the margin collateral oracle price.
     /// @param self The margin collateral type storage pointer.
     /// @param priceFeed The Chainlink Price Feed address.
-    /// @return price The parsed price of the given margin collateral type.
+    /// @return price The price of the given margin collateral type.
     function getPrice(Data storage self, IAggregatorV3 priceFeed) internal view returns (UD60x18 price) {
+        uint8 decimals = self.decimals;
         uint8 priceDecimals = priceFeed.decimals();
         (, int256 answer,,,) = priceFeed.latestRoundData();
 
