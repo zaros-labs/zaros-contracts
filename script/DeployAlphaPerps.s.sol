f246c0ccd7a49643a8ff8b2810392412b74467ad
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-01-24 14:53:23 -0300

    chore: get module selectors helper

--- script/DeployAlphaPerps.s.sol
+++ script/DeployAlphaPerps.s.sol
@@ -52,7 +52,6 @@ contract DeployAlphaPerps is BaseScript {
             address settlementModule
         ) = deployModules();
 
-        // perpsEngineImplementation = new PerpsEngine();
         bytes memory initializeData = abi.encodeWithSelector(
             perpsEngineImplementation.initialize.selector,
             deployer,
@@ -61,6 +60,12 @@ contract DeployAlphaPerps is BaseScript {
             address(usdToken),
             mockZarosAddress
         );
+        IDiamond.FacetCut[] memory facetCuts = new IDiamond.FacetCut[](5);
+        // facetCuts[0] = IDiamond.FacetCut({
+        //     facet: globalConfigurationModule,
+        //     action: IDiamond.FacetCutAction.Add,
+        //     selectors: new bytes4[](0)
+        // });
 
         // (bool success,) = address(perpsEngineImplementation).call(initializeData);
         // require(success, "perpsEngineImplementation.initialize failed");
@@ -80,6 +85,74 @@ contract DeployAlphaPerps is BaseScript {
         address settlementModule = address(new SettlementModule());
 
         return (globalConfigurationModule, orderModule, perpMarketModule, perpsAccountModule, settlementModule);
+    }
+
+    function getModuleSelectors() internal pure returns (bytes4[][5]) {
+        bytes4[][5] memory selectors = new bytes4[][5](5);
+
+        bytes4[] memory globalConfigurationModuleSelectors = new bytes4[](9);
+
+        globalConfigurationModuleSelectors[0] =
+            GlobalConfigurationModule.getDepositCapForMarginCollateralConfiguration.selector;
+        globalConfigurationModuleSelectors[1] = GlobalConfigurationModule.setPerpsAccountToken.selector;
+        globalConfigurationModuleSelectors[2] = GlobalConfigurationModule.setLiquidityEngine.selector;
+        globalConfigurationModuleSelectors[3] = GlobalConfigurationModule.configureMarginCollateral.selector;
+        globalConfigurationModuleSelectors[4] = GlobalConfigurationModule.configureCollateralPriority.selector;
+        globalConfigurationModuleSelectors[5] = GlobalConfigurationModule.removeCollateralFromPriorityList.selector;
+        globalConfigurationModuleSelectors[6] = GlobalConfigurationModule.configureSystemParameters.selector;
+        globalConfigurationModuleSelectors[7] = GlobalConfigurationModule.createPerpMarket.selector;
+        globalConfigurationModuleSelectors[8] = GlobalConfigurationModule.upatePerpMarketStatus.selector;
+
+        bytes4[] memory orderModuleSelectors = new bytes4[](7);
+
+        orderModuleSelectors[0] = OrderModule.getConfiguredOrderFees.selector;
+        orderModuleSelectors[1] = OrderModule.estimateOrderFee.selector;
+        orderModuleSelectors[2] = OrderModule.getRequiredMarginForOrder.selector;
+        orderModuleSelectors[3] = OrderModule.getActiveMarketOrder.selector;
+        orderModuleSelectors[4] = OrderModule.createMarketOrder.selector;
+        orderModuleSelectors[5] = OrderModule.dispatchCustomSettlementRequest.selector;
+        orderModuleSelectors[6] = OrderModule.cancelMarketOrder.selector;
+
+        bytes4[] memory perpMarketModuleSelectors = new bytes4[](11);
+
+        perpMarketModuleSelectors[0] = PerpMarketModule.name.selector;
+        perpMarketModuleSelectors[1] = PerpMarketModule.symbol.selector;
+        perpMarketModuleSelectors[2] = PerpMarketModule.getMaxOpenInterest.selector;
+        perpMarketModuleSelectors[3] = PerpMarketModule.getSkew.selector;
+        perpMarketModuleSelectors[4] = PerpMarketModule.getOpenInterest.selector;
+        perpMarketModuleSelectors[5] = PerpMarketModule.getMarkPrice.selector;
+        perpMarketModuleSelectors[6] = PerpMarketModule.getSettlementConfiguration.selector;
+        perpMarketModuleSelectors[7] = PerpMarketModule.getFundingRate.selector;
+        perpMarketModuleSelectors[8] = PerpMarketModule.getFundingVelocity.selector;
+        perpMarketModuleSelectors[9] = PerpMarketModule.getAccountLeverage.selector;
+        perpMarketModuleSelectors[10] = PerpMarketModule.getMarketData.selector;
+
+        bytes4[] memory perpsAccountModuleSelectors = new bytes4[](11);
+
+        perpsAccountModuleSelectors[0] = PerpsAccountModule.getPerpsAccountToken.selector;
+        perpsAccountModuleSelectors[1] = PerpsAccountModule.getAccountMarginCollateralBalance.selector;
+        perpsAccountModuleSelectors[2] = PerpsAccountModule.getAccountEquityUsd.selector;
+        perpsAccountModuleSelectors[3] = PerpsAccountModule.getAccountMarginBreakdown.selector;
+        perpsAccountModuleSelectors[4] = PerpsAccountModule.getAccountTotalUnrealizedPnl.selector;
+        perpsAccountModuleSelectors[5] = PerpsAccountModule.getOpenPositionData.selector;
+        perpsAccountModuleSelectors[6] = PerpsAccountModule.createPerpsAccount.selector;
+        perpsAccountModuleSelectors[7] = PerpsAccountModule.createPerpsAccountAndMulticall.selector;
+        perpsAccountModuleSelectors[8] = PerpsAccountModule.depositMargin.selector;
+        perpsAccountModuleSelectors[9] = PerpsAccountModule.withdrawMargin.selector;
+        perpsAccountModuleSelectors[10] = PerpsAccountModule.notifyAccountTransfer.selector;
+
+        bytes4[] memory settlementModuleSelectors = new bytes4[](2);
+
+        settlementModuleSelectors[0] = SettlementModule.settleMarketOrder.selector;
+        settlementModuleSelectors[1] = SettlementModule.settleCustomTriggers.selector;
+
+        selectors[0] = globalConfigurationModuleSelectors;
+        selectors[1] = orderModuleSelectors;
+        selectors[2] = perpMarketModuleSelectors;
+        selectors[3] = perpsAccountModuleSelectors;
+        selectors[4] = settlementModuleSelectors;
+
+        return selectors;
     }
 
     function configureContracts() internal {
