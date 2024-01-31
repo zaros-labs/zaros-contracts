7e64b46f7878a9c9be83238f43b4a5ecf868e915
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-01-31 15:31:33 -0300

    script: update modules

--- script/UpdateModules.s.sol
+++ script/UpdateModules.s.sol
@@ -6,43 +6,26 @@ pragma solidity 0.8.23;
 import { AccountNFT } from "@zaros/account-nft/AccountNFT.sol";
 import { IDiamond } from "@zaros/diamonds/interfaces/IDiamond.sol";
 import { Diamond } from "@zaros/diamonds/Diamond.sol";
+import { PerpsEngine } from "@zaros/markets/perps/PerpsEngine.sol";
 import { IPerpsEngine } from "@zaros/markets/perps/interfaces/IPerpsEngine.sol";
 import { OrderFees } from "@zaros/markets/perps/storage/OrderFees.sol";
 import { USDToken } from "@zaros/usd/USDToken.sol";
 import { BaseScript } from "./Base.s.sol";
-import {
-    deployModules,
-    getModulesSelectors,
-    getFacetCuts,
-    getInitializables,
-    getInitializePayloads
-} from "script/utils/DiamondHelpers.sol";
+import { deployModules, getModulesSelectors, getFacetCuts } from "./utils/DiamondHelpers.sol";
 
-// Open Zeppelin Upgradeable dependencies
+// Open Zeppelin dependencies
 import { ERC1967Proxy } from "@openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
 
 // Forge dependencies
 import "forge-std/console.sol";
 
-contract DeployAlphaPerps is BaseScript {
-    /*//////////////////////////////////////////////////////////////////////////
-                                     VARIABLES
-    //////////////////////////////////////////////////////////////////////////*/
-    address internal mockChainlinkForwarder = address(1);
-    address internal mockChainlinkVerifier = address(2);
-    address internal mockPerpsAccountTokenAddress = address(3);
-    address internal mockRewardDistributorAddress = address(4);
-    address internal mockLiquidityEngineAddress = address(5);
-
+contract UpdateModules is BaseScript {
     /*//////////////////////////////////////////////////////////////////////////
                                     CONTRACTS
     //////////////////////////////////////////////////////////////////////////*/
-    USDToken internal usdToken;
     IPerpsEngine internal perpsEngine;
 
     function run() public broadcaster {
-        usdToken = USDToken(vm.envAddress("USDZ"));
-
         address[] memory modules = deployModules();
         bytes4[][] memory modulesSelectors = getModulesSelectors();
 
@@ -51,7 +34,8 @@ contract DeployAlphaPerps is BaseScript {
         address[] memory initializables;
         bytes[] memory initializePayloads;
 
-        perpsEngine = IPerpsEngine(payable(vm.envAddress("PERPS_ENGINE")));
+        perpsEngine = IPerpsEngine(vm.envAddress("PERPS_ENGINE_PROXY"));
+
         perpsEngine.updateModules(facetCuts, initializables, initializePayloads);
 
         logContracts(modules);
@@ -59,7 +43,7 @@ contract DeployAlphaPerps is BaseScript {
 
     function logContracts(address[] memory modules) internal view {
         for (uint256 i = 0; i < modules.length; i++) {
-            console.log("New Module: ");
+            console.log("Module: ");
             console.log(modules[i]);
         }
 
