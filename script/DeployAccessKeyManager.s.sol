a9785798151e9142b509801203da2dff63afddf7
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2024-02-08 19:18:18 -0300

    refactor: access key manager #59

--- script/DeployAccessKeyManager.s.sol
+++ script/DeployAccessKeyManager.s.sol
@@ -4,7 +4,10 @@ pragma solidity 0.8.23;
 
 // Zaros dependencies
 import { BaseScript } from "./Base.s.sol";
-import { AccessKeyManager } from "@zaros/access-key-manager/AccessKeyManager.sol";
+import { AccessKeyManager } from "../testnet/access-key-manager/AccessKeyManager.sol";
+
+// Open zeppelin upgradeable dependencies
+import { ERC1967Proxy } from "@openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
 
 // Forge dependencies
 import "forge-std/console.sol";
@@ -12,9 +15,15 @@ import "forge-std/console.sol";
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract DeployAccessKeyManager is BaseScript {
     function run() public broadcaster {
-        address accessKeyManager = address(new AccessKeyManager(vm.envAddress("SPEARMINT_ADDRESS")));
+        address accessKeyManagerImplementation = address(new AccessKeyManager());
 
-        console.log("AccessKeyManager: ");
-        console.log(accessKeyManager);
+        bytes memory initializeData =
+            abi.encodeWithSelector(AccessKeyManager.initialize.selector, deployer, vm.envAddress("SPEARMINT_SIGNER"));
+
+        address accessKeyManagerProxy =
+            address(new ERC1967Proxy(accessKeyManagerImplementation, initializeData));
+
+        console.log("Access Key Manager Implementation: ", accessKeyManagerImplementation);
+        console.log("Access Key Manager Proxy: ", accessKeyManagerProxy);
     }
 }
---
+++ testnet/access-key-manager/AccessKeyManager.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.23;
+
+// Open zeppelin dependencies
+import "@openzeppelin/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/utils/cryptography/MessageHashUtils.sol";
+import "@openzeppelin/utils/cryptography/SignatureChecker.sol";
+
+// Open zeppelin upgradeable dependencies
+import { ERC20PermitUpgradeable } from "@openzeppelin-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import { OwnableUpgradeable } from "@openzeppelin-upgradeable/access/OwnableUpgradeable.sol";
+import { UUPSUpgradeable } from "@openzeppelin-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract AccessKeyManager is OwnableUpgradeable,  UUPSUpgradeable {
+    using ECDSA for bytes32;
+    using MessageHashUtils for bytes32;
+    using SignatureChecker for address;
+
+    address public spearmintSigner;
+    uint96 internal nextKeyId;
+
+    struct GeneratedKey {
+        uint96 id;
+        bytes16 key;
+        uint256 timestamp;
+    }
+
+    mapping (address user => GeneratedKey[] keys) userGeneratedKeys;
+    mapping (bytes16 key => bool status) usedKeys;
+    mapping (address user => bool status) usersActive;
+
+    error InvalidSignature();
+
+    function initialize(address owner, address _spearmintSigner) external initializer {
+        spearmintSigner = _spearmintSigner;
+
+        __Ownable_init(owner);
+    }
+
+    function createKey(bytes calldata _signature) external {
+        _validateSignature(_signature);
+
+        bytes16 key = bytes16(keccak256(abi.encode(msg.sender, ++nextKeyId)));
+
+        GeneratedKey memory generatedKey;
+        generatedKey.id = nextKeyId;
+        generatedKey.key = key;
+        generatedKey.timestamp = block.timestamp;
+
+        userGeneratedKeys[msg.sender].push(generatedKey);
+    }
+
+    function getKeysByUser() external view returns (GeneratedKey[] memory) {
+        return userGeneratedKeys[msg.sender];
+    }
+
+    function activateKey(bytes16 key) external {
+        require(!usedKeys[key], "Your key is already used");
+
+        usedKeys[key] = true;
+        usersActive[msg.sender] = true;
+    }
+
+    function userHasAccess() external view returns (bool) {
+        return usersActive[msg.sender];
+    }
+
+    function isKeyValid(bytes16 key) external view returns (bool) {
+        return usedKeys[key];
+    }
+
+    function setSpearmintSigner(address _spearmintSigner) public onlyOwner {
+        spearmintSigner = _spearmintSigner;
+    }
+
+     function _validateSignature(
+        bytes calldata signature
+    ) internal view {
+        bytes32 hashedData = keccak256(abi.encode(msg.sender));
+
+        if (
+            !spearmintSigner.isValidSignatureNow(
+                hashedData.toEthSignedMessageHash(),
+                signature
+            )
+        ) {
+            revert InvalidSignature();
+        }
+    }
+
+    function _authorizeUpgrade(address) internal override onlyOwner { }
+}
