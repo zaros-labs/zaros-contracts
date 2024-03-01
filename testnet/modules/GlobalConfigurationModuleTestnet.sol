eda5743e76edb19b86826ee9d536b1c5ca615f89
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-03-01 20:27:31 -0300

    chore: log custom referral creation

--- testnet/modules/GlobalConfigurationModuleTestnet.sol
+++ testnet/modules/GlobalConfigurationModuleTestnet.sol
@@ -8,11 +8,15 @@ import { Points } from "../storage/Points.sol";
 
 
 contract GlobalConfigurationModuleTestnet is GlobalConfigurationModule {
+    event LogCreateCustomReferralCode(address indexed referrer, string customReferralCode);
+
     function setUserPoints(address user, uint256 value) external onlyOwner {
         Points.load(user).amount = value;
     }
 
     function createCustomReferralCode(address referrer, string memory customReferralCode) external onlyOwner {
         CustomReferralConfigurationTestnet.load(customReferralCode).referrer = referrer;
+
+        emit LogCreateCustomReferralCode(referrer, customReferralCode);
     }
 }
