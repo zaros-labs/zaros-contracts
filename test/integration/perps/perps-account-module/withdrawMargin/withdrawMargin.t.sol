eb76a1702c13283a3799bddd1704e8efe8726522
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-03-27 17:06:06 -0300

    test: progress on testFuzz_RevertGiven_TheAccountWontMeetTheMarginRequirements

--- test/integration/perps/perps-account-module/withdrawMargin/withdrawMargin.t.sol
+++ test/integration/perps/perps-account-module/withdrawMargin/withdrawMargin.t.sol
@@ -5,6 +5,8 @@ pragma solidity 0.8.23;
 // Zaros dependencies
 import { Base_Integration_Shared_Test } from "test/integration/shared/BaseIntegration.t.sol";
 import { Errors } from "@zaros/utils/Errors.sol";
+import { IOrderModule } from "@zaros/markets/perps/interfaces/IOrderModule.sol";
+import { SettlementConfiguration } from "@zaros/markets/perps/storage/SettlementConfiguration.sol";
 
 // PRB Math dependencies
 import { ud60x18, ZERO as UD_ZERO } from "@prb-math/UD60x18.sol";
@@ -12,6 +14,9 @@ import { ud60x18, ZERO as UD_ZERO } from "@prb-math/UD60x18.sol";
 contract WithdrawMargin_Integration_Test is Base_Integration_Shared_Test {
     function setUp() public override {
         Base_Integration_Shared_Test.setUp();
+        changePrank({ msgSender: users.owner });
+        configureSystemParameters();
+        createMarkets();
         changePrank({ msgSender: users.naruto });
     }
 
@@ -38,15 +43,17 @@ contract WithdrawMargin_Integration_Test is Base_Integration_Shared_Test {
         external
         givenTheAccountExists
     {
-        amountToDeposit = bound({ x: amountToDeposit, min: 1, max: USDZ_DEPOSIT_CAP });
-        amountToWithdraw = bound({ x: amountToWithdraw, min: 1, max: amountToDeposit });
+        amountToDeposit = bound({ x: amountToDeposit, min: USDZ_MIN_DEPOSIT_MARGIN, max: USDZ_DEPOSIT_CAP });
+        amountToWithdraw = bound({ x: amountToWithdraw, min: USDZ_MIN_DEPOSIT_MARGIN, max: amountToDeposit });
         deal({ token: address(usdToken), to: users.naruto, give: amountToDeposit });
 
         uint128 perpsAccountId = createAccountAndDeposit(amountToDeposit, address(usdToken));
         changePrank({ msgSender: users.sasuke });
 
         // it should revert
-        vm.expectRevert({ revertData: abi.encodeWithSelector(Errors.AccountPermissionDenied.selector, perpsAccountId, users.sasuke) });
+        vm.expectRevert({
+            revertData: abi.encodeWithSelector(Errors.AccountPermissionDenied.selector, perpsAccountId, users.sasuke)
+        });
         perpsEngine.withdrawMargin(perpsAccountId, address(usdToken), ud60x18(amountToWithdraw));
     }
 
@@ -59,7 +66,7 @@ contract WithdrawMargin_Integration_Test is Base_Integration_Shared_Test {
         givenTheAccountExists
         givenTheSenderIsAuthorized
     {
-        amountToDeposit = bound({ x: amountToDeposit, min: 1, max: USDZ_DEPOSIT_CAP });
+        amountToDeposit = bound({ x: amountToDeposit, min: USDZ_MIN_DEPOSIT_MARGIN, max: USDZ_DEPOSIT_CAP });
         deal({ token: address(usdToken), to: users.naruto, give: amountToDeposit });
 
         uint128 perpsAccountId = createAccountAndDeposit(amountToDeposit, address(usdToken));
@@ -82,7 +89,7 @@ contract WithdrawMargin_Integration_Test is Base_Integration_Shared_Test {
         givenTheSenderIsAuthorized
         whenTheAmountIsNotZero
     {
-        amountToDeposit = bound({ x: amountToDeposit, min: 1, max: USDZ_DEPOSIT_CAP });
+        amountToDeposit = bound({ x: amountToDeposit, min: USDZ_MIN_DEPOSIT_MARGIN, max: USDZ_DEPOSIT_CAP });
         vm.assume(amountToWithdraw > amountToDeposit);
         uint256 expectedMarginCollateralBalance =
             convertTokenAmountToUd60x18(address(usdToken), amountToDeposit).intoUint256();
@@ -103,14 +110,65 @@ contract WithdrawMargin_Integration_Test is Base_Integration_Shared_Test {
         _;
     }
 
-    function testFuzz_RevertGiven_TheAccountWontMeetTheMarginRequirements()
+    function testFuzz_RevertGiven_TheAccountWontMeetTheMarginRequirements(
+        uint256 amountToDeposit,
+        uint256 amountToWithdraw,
+        uint256 marginRequirement,
+        bool isLong
+    )
         external
         givenTheAccountExists
         givenTheSenderIsAuthorized
         whenTheAmountIsNotZero
         givenThereIsEnoughMarginCollateral
     {
+        amountToDeposit = bound({ x: amountToDeposit, min: USDZ_MIN_DEPOSIT_MARGIN, max: USDZ_DEPOSIT_CAP });
+        marginRequirement =
+            bound({ x: marginRequirement, min: ETH_USD_MARGIN_REQUIREMENTS, max: MAX_MARGIN_REQUIREMENTS });
+        uint256 requiredMarginUsd = ud60x18(amountToDeposit).div(ud60x18(marginRequirement)).mul(
+            ud60x18(ETH_USD_MARGIN_REQUIREMENTS)
+        ).intoUint256();
+        amountToWithdraw = bound({ x: amountToWithdraw, min: requiredMarginUsd, max: amountToDeposit });
+        deal({ token: address(usdToken), to: users.naruto, give: amountToDeposit });
+
+        uint128 perpsAccountId = createAccountAndDeposit(amountToDeposit, address(usdToken));
+        int128 sizeDelta = fuzzOrderSizeDelta(
+            FuzzOrderSizeDeltaParams({
+                accountId: perpsAccountId,
+                marketId: ETH_USD_MARKET_ID,
+                settlementId: SettlementConfiguration.MARKET_ORDER_CONFIGURATION_ID,
+                initialMarginRate: ud60x18(marginRequirement),
+                marginValueUsd: ud60x18(amountToDeposit),
+                maxOpenInterest: ud60x18(ETH_USD_MAX_OI),
+                minTradeSize: ud60x18(ETH_USD_MIN_TRADE_SIZE),
+                price: ud60x18(MOCK_ETH_USD_PRICE),
+                isLong: isLong
+            })
+        );
+
+        perpsEngine.createMarketOrder(
+            IOrderModule.CreateMarketOrderParams({
+                accountId: perpsAccountId,
+                marketId: ETH_USD_MARKET_ID,
+                sizeDelta: sizeDelta,
+                acceptablePrice: 0
+            })
+        );
+
+        changePrank({ msgSender: mockDefaultMarketOrderSettlementStrategy });
+        bytes memory mockBasicSignedReport = getMockedSignedReport(MOCK_ETH_USD_STREAM_ID, MOCK_ETH_USD_PRICE, false);
+
+        mockSettleMarketOrder(perpsAccountId, ETH_USD_MARKET_ID, mockBasicSignedReport);
+
+        changePrank({ msgSender: users.naruto });
+
+        vm.expectRevert({
+            revertData: abi.encodeWithSelector(
+                Errors.InsufficientMargin.selector, perpsAccountId, amountToDeposit - amountToWithdraw, requiredMarginUsd
+                )
+        });
         // it should revert
+        perpsEngine.withdrawMargin(perpsAccountId, address(usdToken), ud60x18(amountToWithdraw));
     }
 
     function testFuzz_GivenTheAccountMeetsTheMarginRequirements(
