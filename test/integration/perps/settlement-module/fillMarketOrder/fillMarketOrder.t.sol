14a21f4e63c606efdd3bbaebdb0c36524dd860e3
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-04-10 11:52:58 -0300

    chore: create fillMarketOrder test directory

---
+++ test/integration/perps/settlement-module/fillMarketOrder/fillMarketOrder.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.23;
+
+// Zaros dependencies
+import { BasicReport } from "@zaros/external/chainlink/interfaces/IStreamsLookupCompatible.sol";
+import { MarketOrder } from "@zaros/markets/perps/storage/MarketOrder.sol";
+import { Position } from "@zaros/markets/perps/storage/Position.sol";
+import { SettlementConfiguration } from "@zaros/markets/perps/storage/SettlementConfiguration.sol";
+import { Base_Integration_Shared_Test } from "test/integration/shared/BaseIntegration.t.sol";
+
+// PRB Math dependencies
+import { ud60x18 } from "@prb-math/UD60x18.sol";
+import { sd59x18 } from "@prb-math/SD59x18.sol";
+
+contract FillMarketOrder_Integration_Test is Base_Integration_Shared_Test {
+    function setUp() public override {
+        Base_Integration_Shared_Test.setUp();
+        changePrank({ msgSender: users.owner });
+        configureSystemParameters();
+        createMarkets();
+        changePrank({ msgSender: users.naruto });
+    }
+}
