15efba37db13264011a31b8f004ee37b2af23e7f
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-05-08 11:19:12 -0300

    chore: starts openPosition utility

--- test/integration/perps/liquidation-branch/checkLiquidatableAccounts/checkLiquidatableAccounts.t.sol
+++ test/integration/perps/liquidation-branch/checkLiquidatableAccounts/checkLiquidatableAccounts.t.sol
@@ -2,7 +2,12 @@
 pragma solidity 0.8.25;
 
 // Zaros dependencies
+import { IOrderBranch } from "@zaros/perpetuals/interfaces/IOrderBranch.sol";
+import { SettlementConfiguration } from "@zaros/perpetuals/leaves/SettlementConfiguration.sol";
 import { Base_Integration_Shared_Test } from "test/integration/shared/BaseIntegration.t.sol";
+
+// PRB Math dependencies
+import { ud60x18 } from "@prb-math/UD60x18.sol";
 
 contract CheckLiquidatableAccounts_Integration_Test is Base_Integration_Shared_Test {
     function setUp() public override {
@@ -13,5 +18,69 @@ contract CheckLiquidatableAccounts_Integration_Test is Base_Integration_Shared_T
         createPerpMarkets();
 
         changePrank({ msgSender: users.naruto });
+    }
+
+    function test_WhenTheBoundsAreZero(uint256 marketId, uint256 marginValueUsd, bool isLong) external {
+        MarketConfig memory fuzzMarketConfig = getFuzzMarketConfig(marketId);
+        marginValueUsd = bound({ x: marginValueUsd, min: USDZ_MIN_DEPOSIT_MARGIN, max: USDZ_DEPOSIT_CAP });
+
+        deal({ token: address(usdToken), to: users.naruto, give: marginValueUsd });
+
+        uint128 tradingAccountId = createAccountAndDeposit(marginValueUsd, address(usdToken));
+        // it should return an empty array
+    }
+
+    function test_WhenTheresNoLiquidatableAccount() external {
+        // it should return an empty array
+    }
+
+    function test_WhenThereAreOneOrManyLiquidatableAccounts() external {
+        // it should return an array with the liquidatable accounts ids
+    }
+
+    function openPosition(
+        MarketConfig memory fuzzMarketConfig,
+        uint128 tradingAccountId,
+        uint256 initialMarginRate,
+        uint256 marginValueUsd,
+        bool isLong
+    )
+        private
+    {
+        address marketOrderKeeper = marketOrderKeepers[fuzzMarketConfig.marketId];
+
+        deal({ token: address(usdToken), to: users.naruto, give: marginValueUsd });
+
+        int128 sizeDelta = fuzzOrderSizeDelta(
+            FuzzOrderSizeDeltaParams({
+                tradingAccountId: tradingAccountId,
+                marketId: fuzzMarketConfig.marketId,
+                settlementConfigurationId: SettlementConfiguration.MARKET_ORDER_CONFIGURATION_ID,
+                initialMarginRate: ud60x18(initialMarginRate),
+                marginValueUsd: ud60x18(marginValueUsd),
+                maxOpenInterest: ud60x18(fuzzMarketConfig.maxOi),
+                minTradeSize: ud60x18(fuzzMarketConfig.minTradeSize),
+                price: ud60x18(fuzzMarketConfig.mockUsdPrice),
+                isLong: isLong,
+                shouldDiscountFees: true
+            })
+        );
+
+        // first market order
+        perpsEngine.createMarketOrder(
+            IOrderBranch.CreateMarketOrderParams({
+                tradingAccountId: tradingAccountId,
+                marketId: fuzzMarketConfig.marketId,
+                sizeDelta: sizeDelta
+            })
+        );
+
+        bytes memory mockSignedReport =
+            getMockedSignedReport(fuzzMarketConfig.streamId, fuzzMarketConfig.mockUsdPrice);
+
+        changePrank({ msgSender: marketOrderKeeper });
+
+        // fill first order and open position
+        perpsEngine.fillMarketOrder(tradingAccountId, fuzzMarketConfig.marketId, feeRecipients, mockSignedReport);
     }
 }
