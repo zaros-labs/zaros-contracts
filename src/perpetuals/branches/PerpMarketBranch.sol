63ac81a2922d802dd089624b8da0943b4bd65264
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2024-05-08 07:45:46 -0300

    refactor: merge IPerpMarketBranch with branches #223

--- src/perpetuals/branches/PerpMarketBranch.sol
+++ src/perpetuals/branches/PerpMarketBranch.sol
@@ -3,7 +3,6 @@
 pragma solidity 0.8.25;
 
 // Zaros dependencies
-import { IPerpMarketBranch } from "../interfaces/IPerpMarketBranch.sol";
 import { OrderFees } from "../leaves/OrderFees.sol";
 import { Position } from "../leaves/Position.sol";
 import { PerpMarket } from "../leaves/PerpMarket.sol";
@@ -15,36 +14,46 @@ import { SD59x18, sd59x18, unary, ZERO as SD_ZERO } from "@prb-math/SD59x18.sol"
 
 import { console } from "forge-std/console.sol";
 
-/// @notice See {IPerpMarketBranch}.
-contract PerpMarketBranch is IPerpMarketBranch {
+/// @title Perps Engine Branch.
+/// @notice The perps engine  is responsible by the state management of perps markets.
+contract PerpMarketBranch {
     using PerpMarket for PerpMarket.Data;
     using Position for Position.Data;
 
-    /// @inheritdoc IPerpMarketBranch
-    function getName(uint128 marketId) external view override returns (string memory) {
+    /// @notice Returns the given perps market name.
+    /// @param marketId The perps market id.
+    function getName(uint128 marketId) external view returns (string memory) {
         return PerpMarket.load(marketId).configuration.name;
     }
 
-    /// @inheritdoc IPerpMarketBranch
-    function getSymbol(uint128 marketId) external view override returns (string memory) {
+    /// @notice Returns the given perps market symbol.
+    /// @param marketId The perps market id.
+    function getSymbol(uint128 marketId) external view returns (string memory) {
         return PerpMarket.load(marketId).configuration.symbol;
     }
 
-    /// @inheritdoc IPerpMarketBranch
-    function getMaxOpenInterest(uint128 marketId) external view override returns (UD60x18) {
+    /// @notice Returns the maximum open interest on a side of the given market.
+    /// @param marketId The perps market id.
+    function getMaxOpenInterest(uint128 marketId) external view returns (UD60x18) {
         return ud60x18(PerpMarket.load(marketId).configuration.maxOpenInterest);
     }
 
-    /// @inheritdoc IPerpMarketBranch
-    function getSkew(uint128 marketId) public view override returns (SD59x18) {
+    /// @notice Returns the current market skew.
+    /// @param marketId The perps market id.
+    function getSkew(uint128 marketId) public view returns (SD59x18) {
         return sd59x18(PerpMarket.load(marketId).skew);
     }
 
-    /// @inheritdoc IPerpMarketBranch
+    /// @notice Returns the given market's open interest, including the size of longs and shorts.
+    /// @dev E.g: There is 500 ETH in long positions and 450 ETH in short positions, this function
+    /// should return UD60x18 longsOpenInterest = 500e18 and UD60x18 shortsOpenInterest = 450e18;
+    /// @param marketId The perps market id.
+    /// @return longsOpenInterest The open interest in long positions.
+    /// @return shortsOpenInterest The open interest in short positions.
+    /// @return totalOpenInterest The sum of longsOpenInterest and shortsOpenInterest.
     function getOpenInterest(uint128 marketId)
         external
         view
-        override
         returns (UD60x18 longsOpenInterest, UD60x18 shortsOpenInterest, UD60x18 totalOpenInterest)
     {
         PerpMarket.Data storage perpMarket = PerpMarket.load(marketId);
@@ -67,46 +76,54 @@ contract PerpMarketBranch is IPerpMarketBranch {
         totalOpenInterest = longsOpenInterest.add(shortsOpenInterest);
     }
 
-    /// @inheritdoc IPerpMarketBranch
-    function getMarkPrice(
-        uint128 marketId,
-        uint256 indexPrice,
-        int256 skewDelta
-    )
-        external
-        view
-        override
-        returns (UD60x18)
-    {
+    /// @notice Returns the given market's mark price based on the offchain price.
+    /// @dev It returns the adjusted price if the market's skew is being updated.
+    /// @param marketId The perps market id.
+    /// @param indexPrice The offchain index price.
+    /// @param skewDelta The size of the skew update.
+    /// @return markPrice The market's mark price.
+    function getMarkPrice(uint128 marketId, uint256 indexPrice, int256 skewDelta) external view returns (UD60x18) {
         PerpMarket.Data storage perpMarket = PerpMarket.load(marketId);
 
         return perpMarket.getMarkPrice(sd59x18(skewDelta), ud60x18(indexPrice));
     }
 
-    /// @inheritdoc IPerpMarketBranch
+    /// @notice Returns a Settlement Strategy used by the given market.
+    /// @param marketId The perps market id.
+    /// @param settlementConfigurationId The perps market settlement configuration id
     function getSettlementConfiguration(
         uint128 marketId,
         uint128 settlementConfigurationId
     )
         external
         pure
-        override
         returns (SettlementConfiguration.Data memory)
     {
         return SettlementConfiguration.load(marketId, settlementConfigurationId);
     }
 
-    /// @inheritdoc IPerpMarketBranch
-    function getFundingRate(uint128 marketId) external view override returns (SD59x18) {
+    /// @notice Returns the given market's funding rate.
+    /// @param marketId The perps market id.
+    function getFundingRate(uint128 marketId) external view returns (SD59x18) {
         return PerpMarket.load(marketId).getCurrentFundingRate();
     }
 
-    /// @inheritdoc IPerpMarketBranch
-    function getFundingVelocity(uint128 marketId) external view override returns (SD59x18) {
+    /// @notice Returns the given market's funding velocity.
+    /// @param marketId The perps market id.
+    function getFundingVelocity(uint128 marketId) external view returns (SD59x18) {
         return PerpMarket.load(marketId).getCurrentFundingVelocity();
     }
 
-    /// @inheritdoc IPerpMarketBranch
+    /// @notice Returns the most relevant data of the given market.
+    /// @param marketId The perps market id.
+    /// @return name The market name.
+    /// @return symbol The market symbol.
+    /// @return initialMarginRateX18 The minimum initial margin rate for the market.
+    /// @return maintenanceMarginRateX18 The maintenance margin rate for the market.
+    /// @return maxOpenInterest The maximum open interest for the market.
+    /// @return skewScale The configured skew scale of the market.
+    /// @return minTradeSizeX18 The minimum trade size of the market.
+    /// @return orderFees The configured maker and taker order fees of the market.
     function getPerpMarketConfiguration(uint128 marketId)
         external
         view
