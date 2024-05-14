34d1e60a5d6341558447a0afa6706a70aebf87ab
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-05-14 11:45:48 -0300

    chore: update max funding velocity

--- script/markets/ArbUsd.sol
+++ script/markets/ArbUsd.sol
@@ -18,7 +18,7 @@ abstract contract ArbUsd {
     uint128 internal constant ARB_USD_MMR = 0.01e18;
     uint128 internal constant ARB_USD_MARGIN_REQUIREMENTS = ARB_USD_IMR + ARB_USD_MMR;
     uint128 internal constant ARB_USD_MAX_OI = 100_000_000e18;
-    uint128 internal constant ARB_USD_MAX_FUNDING_VELOCITY = 0.25e18;
+    uint128 internal constant ARB_USD_MAX_FUNDING_VELOCITY = 0.03e18;
     uint256 internal constant ARB_USD_SKEW_SCALE = 100_243_316_353e18;
     uint256 internal constant ARB_USD_MIN_TRADE_SIZE = 40e18;
     OrderFees.Data internal arbUsdOrderFees = OrderFees.Data({ makerFee: 0.008e18, takerFee: 0.016e18 });
