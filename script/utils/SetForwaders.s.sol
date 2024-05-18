36664d20ba4bb74e3da0ee17a139da938ceed234
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-05-18 16:33:46 -0300

    script: set forwarders

--- script/utils/SetForwaders.s.sol
+++ script/utils/SetForwaders.s.sol
@@ -3,32 +3,25 @@
 pragma solidity 0.8.25;
 
 // Zaros dependencies
-import { IVerifierProxy } from "@zaros/external/chainlink/interfaces/IVerifierProxy.sol";
-import { IPerpsEngine } from "@zaros/perpetuals/PerpsEngine.sol";
-import { SettlementConfiguration } from "@zaros/perpetuals/leaves/SettlementConfiguration.sol";
+import { BaseKeeper } from "@zaros/external/chainlink/keepers/BaseKeeper.sol";
 import { BaseScript } from "script/Base.s.sol";
-import { ProtocolConfiguration } from "script/utils/ProtocolConfiguration.sol";
-
-// Open Zeppelin dependencies
-import { ERC1967Proxy } from "@openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
-import { UUPSUpgradeable } from "@openzeppelin/proxy/utils/UUPSUpgradeable.sol";
 
 // Forge dependencies
 import { console } from "forge-std/console.sol";
 
-contract SetForwarders is BaseScript, ProtocolConfiguration {
-    /*//////////////////////////////////////////////////////////////////////////
-                                    VARIABLES
-    //////////////////////////////////////////////////////////////////////////*/
-    string internal ethUsdStreamId;
+contract SetForwarders is BaseScript {
+    function run(address[] calldata keepers, address[] calldata forwarders) public broadcaster {
+        // solhint-disable-next-line custom-errors
+        require(keepers.length == forwarders.length, "invalid input length");
 
-    /*//////////////////////////////////////////////////////////////////////////
-                                    CONTRACTS
-    //////////////////////////////////////////////////////////////////////////*/
-    IVerifierProxy internal chainlinkVerifier;
-    address internal ethUsdMarketOrderKeepers;
-    IPerpsEngine internal perpsEngine;
+        for (uint256 i = 0; i < keepers.length; i++) {
+            address target = keepers[i];
+            address forwarder = forwarders[i];
 
-    // TODO: Implement
-    function run() public broadcaster { }
+            console.log("Setting forwarder for", target, "to", forwarder);
+
+            // Set the forwarder
+            BaseKeeper(target).setForwarder(forwarder);
+        }
+    }
 }
