a5b77bfb77051a16d2f54c7fdd15ac336a47bf0c
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2024-05-20 15:33:26 -0300

    test: test_GivenTheresAPositionCreated change maxOpenInterest to maxSkew #262

--- test/integration/perpetuals/trading-account-branch/getAccountEquityUsd/getAccountEquityUsd.t.sol
+++ test/integration/perpetuals/trading-account-branch/getAccountEquityUsd/getAccountEquityUsd.t.sol
@@ -98,7 +98,7 @@ contract GetAccountEquityUsd_Integration_Test is Base_Integration_Shared_Test {
                 settlementConfigurationId: SettlementConfiguration.MARKET_ORDER_CONFIGURATION_ID,
                 initialMarginRate: ud60x18(initialMarginRate),
                 marginValueUsd: ud60x18(marginValueUsd),
-                maxOpenInterest: ud60x18(fuzzMarketConfig.maxOi),
+                maxSkew: ud60x18(fuzzMarketConfig.maxSkew),
                 minTradeSize: ud60x18(fuzzMarketConfig.minTradeSize),
                 price: ud60x18(fuzzMarketConfig.mockUsdPrice),
                 isLong: isLong,
