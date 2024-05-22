fe08856281b545c5d4cad98c996d4a85046e568b
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-05-22 15:34:04 -0300

    chore: MarketOrderHarness

---
+++ test/harnesses/perpetuals/leaves/MarketOrderHarness.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+// Zaros dependencies
+import { MarketOrder } from "@zaros/perpetuals/leaves/MarketOrder.sol";
+
+// PRB Math dependencies
+import { UD60x18 } from "@prb-math/UD60x18.sol";
+import { SD59x18 } from "@prb-math/SD59x18.sol";
+
+contract MarketOrderHarness {
+    function exposed_load(uint128 tradingAccountId) external pure returns (MarketOrder.Data memory) {
+        return MarketOrder.load(tradingAccountId);
+    }
+
+    function exposed_loadExisting(uint128 tradingAccountId) external view returns (MarketOrder.Data memory) {
+        return MarketOrder.loadExisting(tradingAccountId);
+    }
+
+    function exposed_update(uint128 tradingAccountId, uint128 marketId, int128 sizeDelta) external {
+        MarketOrder.Data storage self = MarketOrder.load(tradingAccountId);
+        MarketOrder.update(self, marketId, sizeDelta);
+    }
+
+    function exposed_clear(uint128 tradingAccountId) external {
+        MarketOrder.Data storage self = MarketOrder.load(tradingAccountId);
+        MarketOrder.clear(self);
+    }
+
+    function exposed_checkPendingOrder(uint128 tradingAccountId) external view {
+        MarketOrder.Data storage self = MarketOrder.load(tradingAccountId);
+        MarketOrder.checkPendingOrder(self);
+    }
+}
