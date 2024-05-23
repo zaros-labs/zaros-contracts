010378325188cfbc89993b3ffc04dad504c299a9
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-05-23 15:58:54 -0300

    test: getFundingRate

--- test/integration/perpetuals/perp-market-branch/getFundingRate/getFundingRate.t.sol
+++ test/integration/perpetuals/perp-market-branch/getFundingRate/getFundingRate.t.sol
@@ -2,7 +2,14 @@
 pragma solidity 0.8.25;
 
 // Zaros dependencies
+import { Constants } from "@zaros/utils/Constants.sol";
 import { Base_Integration_Shared_Test } from "test/integration/shared/BaseIntegration.t.sol";
+
+// PRB Math dependencies
+import { convert as ud60x18Convert } from "@prb-math/UD60x18.sol";
+import { SD59x18 } from "@prb-math/SD59x18.sol";
+
+import { console } from "forge-std/console.sol";
 
 contract GetFundingRate_Integration_Test is Base_Integration_Shared_Test {
     function setUp() public override {
@@ -13,8 +20,32 @@ contract GetFundingRate_Integration_Test is Base_Integration_Shared_Test {
         changePrank({ msgSender: users.naruto });
     }
 
-    // function testFuzz_GivenTheresAMarketCreated(uint256 marketId) external {
-    //     MarketConfig memory fuzzMarketConfig = getFuzzMarketConfig(marketId);
+    function testFuzz_GivenTheresAMarketCreated(
+        uint128 marketId,
+        uint256 marginValueUsd,
+        bool isLong,
+        uint256 timeElapsed
+    )
+        external
+    {
+        MarketConfig memory fuzzMarketConfig = getFuzzMarketConfig(marketId);
+        marginValueUsd = bound({ x: marginValueUsd, min: USDZ_MIN_DEPOSIT_MARGIN, max: USDZ_DEPOSIT_CAP });
+        timeElapsed = bound({ x: timeElapsed, min: 1 seconds, max: 365 days });
 
-    // }
+        deal({ token: address(usdToken), to: users.naruto, give: marginValueUsd});
+
+        uint256 initialMarginRate = fuzzMarketConfig.imr;
+        uint128 tradingAccountId = createAccountAndDeposit(marginValueUsd, address(usdToken));
+
+        openPosition(fuzzMarketConfig, tradingAccountId, initialMarginRate, marginValueUsd, isLong);
+
+        skip(timeElapsed);
+
+        // it should return the funding rate
+        SD59x18 fundingRate = perpsEngine.getFundingRate(marketId);
+        SD59x18 fundingVelocity = perpsEngine.getFundingVelocity(marketId);
+        int256 expectedFundingRate = fundingVelocity.mul(ud60x18Convert(timeElapsed).intoSD59x18()).div(ud60x18Convert(Constants.PROPORTIONAL_FUNDING_PERIOD).intoSD59x18()).intoInt256();
+
+        assertEq(fundingRate.intoInt256(), expectedFundingRate);
+    }
 }
