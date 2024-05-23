109135b8523cd9134de40aadf2d3c9374ead50be
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-05-23 15:37:09 -0300

    test: merge liquidation shared test with base

---
+++ test/integration/perpetuals/perp-market-branch/getFundingRate/getFundingRate.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+// Zaros dependencies
+import { Base_Integration_Shared_Test } from "test/integration/shared/BaseIntegration.t.sol";
+
+contract GetFundingRate_Integration_Test is Base_Integration_Shared_Test {
+    function setUp() public override {
+        Base_Integration_Shared_Test.setUp();
+        changePrank({ msgSender: users.owner });
+        configureSystemParameters();
+        createPerpMarkets();
+        changePrank({ msgSender: users.naruto });
+    }
+
+    // function testFuzz_GivenTheresAMarketCreated(uint256 marketId) external {
+    //     MarketConfig memory fuzzMarketConfig = getFuzzMarketConfig(marketId);
+
+    // }
+}
