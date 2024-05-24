cc12695d4271d12d99c39543103ed8e46486b977
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2024-05-24 11:12:21 -0300

    test: add another scenario test_GivenRemoveABranchFunction #252

--- test/integration/tree-proxy/upgrade-branch/upgrade.t.sol
+++ test/integration/tree-proxy/upgrade-branch/upgrade.t.sol
@@ -10,8 +10,13 @@ import { IPerpsEngine } from "@zaros/perpetuals/PerpsEngine.sol";
 import { OrderBranch } from "@zaros/perpetuals/branches/OrderBranch.sol";
 import { PerpsEngine } from "@zaros/perpetuals/PerpsEngine.sol";
 
+import { console } from "forge-std/console.sol";
+import { console2 } from "forge-std/console2.sol";
+
 // Open Zeppelin dependencies
 import { Ownable } from "@openzeppelin/access/Ownable.sol";
+
+import { Branch } from "@zaros/tree-proxy/leaves/Branch.sol";
 
 contract TestContract {
     function testFunction() public pure returns (string memory) {
@@ -101,17 +106,51 @@ contract Upgrade_Integration_Test is Base_Integration_Shared_Test {
     function test_GivenRemoveABranchFunction() external givenIAmTheOwner {
         changePrank({ msgSender: users.owner });
 
+        // When we remove a function that already exists in the branch
+
+        Branch.Data[] memory perpsEngineBranches = perpsEngine.branches();
+
+        address orderBranchAddress;
+
+        for (uint256 i = 0; i < perpsEngineBranches.length; i++) {
+            if (i == 4) {
+                orderBranchAddress = perpsEngineBranches[i].branch;
+            }
+        }
+
         address[] memory branches = new address[](1);
+        branches[0] = orderBranchAddress;
+
+        bytes4[][] memory branchesSelectors = new bytes4[][](1);
+        bytes4[] memory orderBranchSelectors = new bytes4[](1);
+        orderBranchSelectors[0] = OrderBranch.getActiveMarketOrder.selector;
+        branchesSelectors[0] = orderBranchSelectors;
+
+        RootProxy.BranchUpgrade[] memory branchUpgrades =
+            getBranchUpgrades(branches, branchesSelectors, RootProxy.BranchUpgradeAction.Remove);
+
+        perpsEngine.upgrade(branchUpgrades, new address[](0), new bytes[](0));
+
+        // it should not return the removed branch functions
+        vm.expectRevert({
+            revertData: abi.encodeWithSelector(
+                Errors.UnsupportedFunction.selector, OrderBranch.getActiveMarketOrder.selector
+            )
+        });
+        perpsEngine.getActiveMarketOrder(1);
+
+        // When we add a new branch and after delete a function from it
+
+        branches = new address[](1);
         address testContract = address(new TestContract());
         branches[0] = testContract;
 
-        bytes4[][] memory branchesSelectors = new bytes4[][](1);
+        branchesSelectors = new bytes4[][](1);
         bytes4[] memory testContractSelectors = new bytes4[](1);
         testContractSelectors[0] = TestContract.testFunction.selector;
         branchesSelectors[0] = testContractSelectors;
 
-        RootProxy.BranchUpgrade[] memory branchUpgrades =
-            getBranchUpgrades(branches, branchesSelectors, RootProxy.BranchUpgradeAction.Add);
+        branchUpgrades = getBranchUpgrades(branches, branchesSelectors, RootProxy.BranchUpgradeAction.Add);
 
         perpsEngine.upgrade(branchUpgrades, new address[](0), new bytes[](0));
         assertEq(PerpsEngineWithNewTestFunction(address(perpsEngine)).testFunction(), "Test");
