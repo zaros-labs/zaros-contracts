790f512e120f64d1dc01076b5ea59a126c29880d
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2024-05-26 11:31:43 -0300

    test: AddMarket_Unit_Test #254

--- test/unit/perpetuals/global-configuration/checkMarketIsEnabled/checkMarketIsEnabled.t.sol
+++ test/unit/perpetuals/global-configuration/checkMarketIsEnabled/checkMarketIsEnabled.t.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.25;
 // Zaros dependencies
 import { Errors } from "@zaros/utils/Errors.sol";
 import { Base_Test } from "test/Base.t.sol";
-import { GlobalConfigurationBranch } from "@zaros/perpetuals/branches/GlobalConfigurationBranch.sol";
 
 contract CheckMarketIsEnabled_Unit_Test is Base_Test {
     function setUp() public override {
@@ -15,7 +14,7 @@ contract CheckMarketIsEnabled_Unit_Test is Base_Test {
         changePrank({ msgSender: users.naruto });
     }
 
-    function testFuzz_RevertWhen_MarketIsNotEnabled(uint256 marketId) external {
+    function testFuzz_RevertWhen_TheMarketIsNotEnabled(uint256 marketId) external {
         marketId = bound({ x: marketId, min: FINAL_MARKET_ID + 1, max: 1_000_000e18 });
 
         // it should revert
@@ -24,7 +23,7 @@ contract CheckMarketIsEnabled_Unit_Test is Base_Test {
         perpsEngine.exposed_checkMarketIsEnabled(uint128(marketId));
     }
 
-    function test_WhenMarketIsEnabled(uint256 marketId) external view {
+    function testFuzz_WhenTheMarketIsEnabled(uint256 marketId) external view {
         marketId = bound({ x: marketId, min: INITIAL_MARKET_ID, max: FINAL_MARKET_ID });
 
         // it should return nothing
