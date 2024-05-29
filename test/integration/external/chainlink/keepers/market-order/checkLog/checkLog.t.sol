542a7c7a4323bb81d37810133e7f54007d000d89
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2024-05-29 09:07:53 -0300

    test: MarketOrderKeeper_CheckLog_Integration_Test #300

--- test/integration/external/chainlink/keepers/market-order/checkLog/checkLog.t.sol
+++ test/integration/external/chainlink/keepers/market-order/checkLog/checkLog.t.sol
@@ -4,18 +4,15 @@ pragma solidity 0.8.25;
 // Zaros dependencies
 import { Errors } from "@zaros/utils/Errors.sol";
 import { Base_Test } from "test/Base.t.sol";
-import { IPerpsEngine } from "@zaros/perpetuals/PerpsEngine.sol";
 import { OrderBranch } from "@zaros/perpetuals/branches/OrderBranch.sol";
 import { SettlementConfiguration } from "@zaros/perpetuals/leaves/SettlementConfiguration.sol";
-import { Markets } from "script/markets/Markets.sol";
 import { Log as AutomationLog } from "@zaros/external/chainlink/interfaces/ILogAutomation.sol";
 import { MarketOrder } from "@zaros/perpetuals/leaves/MarketOrder.sol";
-
 import { MarketOrderKeeper } from "@zaros/external/chainlink/keepers/market-order/MarketOrderKeeper.sol";
+import { IStreamsLookupCompatible } from "@zaros/external/chainlink/interfaces/IStreamsLookupCompatible.sol";
 
 // PRB Math dependencies
-import { UD60x18, ud60x18 } from "@prb-math/UD60x18.sol";
-import { SD59x18, sd59x18, unary } from "@prb-math/SD59x18.sol";
+import { ud60x18 } from "@prb-math/UD60x18.sol";
 
 contract MarketOrderKeeper_CheckLog_Integration_Test is Base_Test {
     function setUp() public override {
@@ -28,82 +25,85 @@ contract MarketOrderKeeper_CheckLog_Integration_Test is Base_Test {
         changePrank({ msgSender: users.naruto });
     }
 
-    modifier givenInitializeContract() {
-        _;
-    }
-
-    function testFuzz_RevertGiven_CallCheckLogFunction(
+    function test_RevertWhen_CheckLogIsCalled(
         uint256 initialMarginRate,
         uint256 marginValueUsd,
         bool isLong,
         uint256 marketId
     )
         external
-        givenInitializeContract
     {
-        // TODO
-        // changePrank({ msgSender: users.naruto });
+        MarketConfig memory fuzzMarketConfig = getFuzzMarketConfig(marketId);
 
-        // MarketConfig memory fuzzMarketConfig = getFuzzMarketConfig(marketId);
+        initialMarginRate =
+            bound({ x: initialMarginRate, min: fuzzMarketConfig.marginRequirements, max: MAX_MARGIN_REQUIREMENTS });
+        marginValueUsd = bound({ x: marginValueUsd, min: USDZ_MIN_DEPOSIT_MARGIN, max: USDZ_DEPOSIT_CAP });
 
-        // initialMarginRate =
-        //     bound({ x: initialMarginRate, min: fuzzMarketConfig.imr, max: MAX_MARGIN_REQUIREMENTS
-        // });
+        deal({ token: address(usdToken), to: users.naruto, give: marginValueUsd });
 
-        // marginValueUsd = bound({ x: marginValueUsd, min: USDZ_MIN_DEPOSIT_MARGIN, max: USDZ_DEPOSIT_CAP });
-        // deal({ token: address(usdToken), to: users.naruto, give: marginValueUsd });
+        uint128 tradingAccountId = createAccountAndDeposit(marginValueUsd, address(usdToken));
+        int128 sizeDelta = fuzzOrderSizeDelta(
+            FuzzOrderSizeDeltaParams({
+                tradingAccountId: tradingAccountId,
+                marketId: fuzzMarketConfig.marketId,
+                settlementConfigurationId: SettlementConfiguration.MARKET_ORDER_CONFIGURATION_ID,
+                initialMarginRate: ud60x18(initialMarginRate),
+                marginValueUsd: ud60x18(marginValueUsd),
+                maxSkew: ud60x18(fuzzMarketConfig.maxSkew),
+                minTradeSize: ud60x18(fuzzMarketConfig.minTradeSize),
+                price: ud60x18(fuzzMarketConfig.mockUsdPrice),
+                isLong: isLong,
+                shouldDiscountFees: true
+            })
+        );
 
-        // uint128 tradingAccountId = createAccountAndDeposit(marginValueUsd, address(usdToken));
-        // int128 sizeDelta = fuzzOrderSizeDelta(
-        //     FuzzOrderSizeDeltaParams({
-        //         tradingAccountId: tradingAccountId,
-        //         marketId: fuzzMarketConfig.marketId,
-        //         settlementConfigurationId: SettlementConfiguration.MARKET_ORDER_CONFIGURATION_ID,
-        //         initialMarginRate: ud60x18(initialMarginRate),
-        //         marginValueUsd: ud60x18(marginValueUsd),
-        //         maxOpenInterest: ud60x18(fuzzMarketConfig.maxOi),
-        // maxSkew: ud60x18(fuzzMarketConfig.maxSkew),
-        //         minTradeSize: ud60x18(fuzzMarketConfig.minTradeSize),
-        //         price: ud60x18(fuzzMarketConfig.mockUsdPrice),
-        //         isLong: isLong,
-        //         shouldDiscountFees: true
-        //     })
-        // );
+        perpsEngine.createMarketOrder(
+            OrderBranch.CreateMarketOrderParams({
+                tradingAccountId: tradingAccountId,
+                marketId: fuzzMarketConfig.marketId,
+                sizeDelta: sizeDelta
+            })
+        );
 
-        // perpsEngine.createMarketOrder(
-        //     OrderBranch.CreateMarketOrderParams({
-        //         tradingAccountId: tradingAccountId,
-        //         marketId: fuzzMarketConfig.marketId,
-        //         sizeDelta: sizeDelta
-        //     })
-        // );
+        // it should return the active market order
+        MarketOrder.Data memory marketOrder = perpsEngine.getActiveMarketOrder(tradingAccountId);
 
-        // bytes memory empty;
+        bytes memory extraData = abi.encode("extraData");
 
-        // bytes32[] memory topics = new bytes32[](4);
-        // topics[0] = keccak256(abi.encode("Log(address,uint128,uint256)"));
-        // topics[1] = keccak256(abi.encode(address(perpsEngine)));
-        // topics[2] = keccak256(abi.encode(tradingAccountId));
-        // topics[3] = keccak256(abi.encode(fuzzMarketConfig.marketId));
+        bytes32[] memory topics = new bytes32[](4);
+        topics[0] = keccak256(abi.encode("Log(address,uint128,uint256)"));
+        topics[1] = keccak256(abi.encode(address(perpsEngine)));
+        topics[2] = bytes32(uint256(tradingAccountId));
+        topics[3] = keccak256(abi.encode(fuzzMarketConfig.marketId));
 
-        // MarketOrder.Data memory marketOrder =
-        //     MarketOrder.Data({ marketId: fuzzMarketConfig.marketId, sizeDelta: sizeDelta, timestamp: 0 });
+        AutomationLog memory mockedLog = AutomationLog({
+            index: 0,
+            timestamp: 0,
+            txHash: 0,
+            blockNumber: 0,
+            blockHash: 0,
+            source: address(0),
+            topics: topics,
+            data: abi.encode(marketOrder)
+        });
 
-        // bytes memory data = abi.encode(marketOrder);
+        address marketOrderKeeper = marketOrderKeepers[fuzzMarketConfig.marketId];
 
-        // AutomationLog memory mockedLog = AutomationLog({
-        //     index: 0,
-        //     timestamp: 0,
-        //     txHash: 0,
-        //     blockNumber: 0,
-        //     blockHash: 0,
-        //     source: address(0),
-        //     topics: topics,
-        //     data: data
-        // });
+        string[] memory streams = new string[](1);
+        streams[0] = fuzzMarketConfig.streamIdString;
 
-        // address marketOrderKeeper = marketOrderKeepers[fuzzMarketConfig.marketId];
         // it should revert
-        // MarketOrderKeeper(marketOrderKeeper).checkLog(mockedLog, empty);
+        vm.expectRevert({
+            revertData: abi.encodeWithSelector(
+                IStreamsLookupCompatible.StreamsLookup.selector,
+                "feedIDs",
+                streams,
+                "timestamp",
+                marketOrder.timestamp,
+                abi.encode(tradingAccountId)
+            )
+        });
+
+        MarketOrderKeeper(marketOrderKeeper).checkLog(mockedLog, extraData);
     }
 }
