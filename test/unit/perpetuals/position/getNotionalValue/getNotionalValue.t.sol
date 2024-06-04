fe2b28c68c4bd8f71f59ead50e1aa956cc5922a1
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2024-06-04 13:43:50 -0300

    test: Position_GetNotionalValue_Unit_Test #260

---
+++ test/unit/perpetuals/position/getNotionalValue/getNotionalValue.t.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+// Zaros dependencies
+import { Errors } from "@zaros/utils/Errors.sol";
+import { Base_Test } from "test/Base.t.sol";
+import { SettlementConfiguration } from "@zaros/perpetuals/leaves/SettlementConfiguration.sol";
+import { OrderBranch } from "@zaros/perpetuals/branches/OrderBranch.sol";
+import { Position } from "@zaros/perpetuals/leaves/Position.sol";
+
+// PRB Math dependencies
+import { UD60x18, ud60x18 } from "@prb-math/UD60x18.sol";
+import { sd59x18 } from "@prb-math/SD59x18.sol";
+
+contract Position_GetNotionalValue_Unit_Test is Base_Test {
+    function setUp() public override {
+        Base_Test.setUp();
+        changePrank({ msgSender: users.owner });
+        configureSystemParameters();
+        createPerpMarkets();
+    }
+
+    function testFuzz_WhenGetNotionalValueIsCalled(
+        uint256 initialMarginRate,
+        uint256 marginValueUsd,
+        bool isLong,
+        uint256 marketId
+    )
+        external
+    {
+        changePrank({ msgSender: users.naruto });
+
+        MarketConfig memory fuzzMarketConfig = getFuzzMarketConfig(marketId);
+
+        initialMarginRate =
+            bound({ x: initialMarginRate, min: fuzzMarketConfig.marginRequirements, max: MAX_MARGIN_REQUIREMENTS });
+
+        marginValueUsd = bound({ x: marginValueUsd, min: USDZ_MIN_DEPOSIT_MARGIN, max: USDZ_DEPOSIT_CAP });
+        deal({ token: address(usdToken), to: users.naruto, give: marginValueUsd });
+
+        uint128 tradingAccountId = createAccountAndDeposit(marginValueUsd, address(usdToken));
+        int128 sizeDelta = fuzzOrderSizeDelta(
+            FuzzOrderSizeDeltaParams({
+                tradingAccountId: tradingAccountId,
+                marketId: fuzzMarketConfig.marketId,
+                settlementConfigurationId: SettlementConfiguration.MARKET_ORDER_CONFIGURATION_ID,
+                initialMarginRate: ud60x18(initialMarginRate),
+                marginValueUsd: ud60x18(marginValueUsd),
+                maxSkew: ud60x18(fuzzMarketConfig.maxSkew),
+                minTradeSize: ud60x18(fuzzMarketConfig.minTradeSize),
+                price: ud60x18(fuzzMarketConfig.mockUsdPrice),
+                isLong: isLong,
+                shouldDiscountFees: true
+            })
+        );
+
+        perpsEngine.createMarketOrder(
+            OrderBranch.CreateMarketOrderParams({
+                tradingAccountId: tradingAccountId,
+                marketId: fuzzMarketConfig.marketId,
+                sizeDelta: sizeDelta
+            })
+        );
+
+        bytes memory mockSignedReport =
+            getMockedSignedReport(fuzzMarketConfig.streamId, fuzzMarketConfig.mockUsdPrice);
+
+        address marketOrderKeeper = marketOrderKeepers[fuzzMarketConfig.marketId];
+
+        changePrank({ msgSender: marketOrderKeeper });
+        perpsEngine.fillMarketOrder(tradingAccountId, fuzzMarketConfig.marketId, mockSignedReport);
+
+        UD60x18 priceX18 = ud60x18(fuzzMarketConfig.mockUsdPrice);
+
+        UD60x18 expectedNotionalValue = sd59x18(sizeDelta).abs().intoUD60x18().mul(priceX18);
+
+        UD60x18 notionalValue =
+            perpsEngine.exposed_getNotionalValue(tradingAccountId, fuzzMarketConfig.marketId, priceX18);
+
+        // it should return the notional value
+        assertEq(
+            notionalValue.intoUint256(),
+            expectedNotionalValue.intoUint256(),
+            "notional value should be equal to expected notional value"
+        );
+    }
+}
