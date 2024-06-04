b11dc4b7a0ab24988ce1ce150763cd34b42a4eea
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2024-06-04 09:56:05 -0300

    test: SetTradingAccountToken_Integration_Test #255

--- test/integration/perpetuals/global-configuration-branch/setTradingAccountToken/setTradingAccountToken.t.sol
+++ test/integration/perpetuals/global-configuration-branch/setTradingAccountToken/setTradingAccountToken.t.sol
@@ -6,12 +6,32 @@ import { Errors } from "@zaros/utils/Errors.sol";
 import { Base_Test } from "test/Base.t.sol";
 import { GlobalConfigurationBranch } from "@zaros/perpetuals/branches/GlobalConfigurationBranch.sol";
 
+// Open Zeppelin dependencies
+import { Ownable } from "@openzeppelin/access/Ownable.sol";
+
 contract SetTradingAccountToken_Integration_Test is Base_Test {
     function setUp() public override {
         Base_Test.setUp();
     }
 
-    function test_RevertGiven_TheTradingAccountTokenIsZero() external {
+    function testFuzz_RevertGiven_TheSenderIsNotTheOwner(address tradingAccountToken) external {
+        changePrank({ msgSender: users.naruto });
+
+        vm.assume(tradingAccountToken != address(0));
+
+        // it should revert
+        vm.expectRevert({
+            revertData: abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, users.naruto)
+        });
+
+        perpsEngine.setTradingAccountToken(tradingAccountToken);
+    }
+
+    modifier givenTheSenderIsTheOwner() {
+        _;
+    }
+
+    function test_RevertWhen_TheTradingAccountTokenIsZero() external givenTheSenderIsTheOwner {
         changePrank({ msgSender: users.owner });
 
         // it should revert
@@ -20,7 +40,10 @@ contract SetTradingAccountToken_Integration_Test is Base_Test {
         perpsEngine.setTradingAccountToken(address(0));
     }
 
-    function testFuzz_GivenTheTradingAccountTokenIsNotAZero(address tradingAccountToken) external {
+    function test_WhenTheTradingAccountTokenIsNotAZero(address tradingAccountToken)
+        external
+        givenTheSenderIsTheOwner
+    {
         changePrank({ msgSender: users.owner });
 
         vm.assume(tradingAccountToken != address(0));
