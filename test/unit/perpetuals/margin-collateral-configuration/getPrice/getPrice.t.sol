7b7056b41f8a09232a49408a39d0a96bf575937d
João Victor Pereira Santos <31141391+0xjoaovpsantos@users.noreply.github.com>
2024-06-11 15:45:08 -0300

    feat: Margin Collateral Configuration #255 (#308)
    * test: MarginCollateralConfiguration_Load_Unit_Test #255
    
    * test: MarginCollateralConfiguration_Configure_Test #255
    
    * refactor: MarginCollateralConfiguration_Configure_Test #255
    
    * test: MarginCollateralConfiguration_ConvertTokenAmountToUd60x18_Test #255
    
    * test: MarginCollateralConfiguration_ConvertUd60x18ToTokenAmount_Test #255
    
    * test: MarginCollateralConfiguration_GetPrice_Test #255
    
    * refactor: added verification to decimals when decimals is zero on configureMarginCollateral #255
    
    * test: add verification on testFuzz_RevertGiven_CollateralThatDoesNotHaveDecimals when have zero decimals #255
    
    * test: SetTradingAccountToken_Integration_Test #255
    
    * test: convertTokenAmountToUd60x18 and convertUd60x18ToTokenAmount #255
    
    * test: verification on storage test_WhenTheTradingAccountTokenIsNotAZero #255

---
+++ test/unit/perpetuals/margin-collateral-configuration/getPrice/getPrice.t.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+// Zaros dependencies
+import { Constants } from "@zaros/utils/Constants.sol";
+import { Errors } from "@zaros/utils/Errors.sol";
+import { Base_Test } from "test/Base.t.sol";
+import { MarginCollateralConfiguration } from "@zaros/perpetuals/leaves/MarginCollateralConfiguration.sol";
+import { MockPriceFeed } from "test/mocks/MockPriceFeed.sol";
+
+// PRB Math dependencies
+import { UD60x18, ud60x18 } from "@prb-math/UD60x18.sol";
+
+contract MarginCollateralConfiguration_GetPrice_Test is Base_Test {
+    function setUp() public virtual override {
+        Base_Test.setUp();
+        changePrank({ msgSender: users.owner });
+        configureSystemParameters();
+        createPerpMarkets();
+        changePrank({ msgSender: users.naruto });
+    }
+
+    function testFuzz_RevertWhen_PriceFeedIsZero(
+        uint128 newDepositCap,
+        uint120 newLoanToValue,
+        uint8 newDecimals
+    )
+        external
+    {
+        address newPriceFeed = address(0);
+
+        perpsEngine.exposed_configure(address(usdToken), newDepositCap, newLoanToValue, newDecimals, newPriceFeed);
+
+        // it should revert
+        vm.expectRevert({ revertData: abi.encodeWithSelector(Errors.CollateralPriceFeedNotDefined.selector) });
+
+        perpsEngine.exposed_getPrice(address(usdToken));
+    }
+
+    function test_WhenPriceFeedIsNotZero() external {
+        UD60x18 price = perpsEngine.exposed_getPrice(address(usdToken));
+
+        MarginCollateralConfiguration.Data memory marginCollateralConfiguration =
+            perpsEngine.exposed_MarginCollateral_load(address(usdToken));
+
+        uint8 priceFeedDecimals = MockPriceFeed(marginCollateralConfiguration.priceFeed).decimals();
+
+        UD60x18 expectedPrice = ud60x18(MOCK_USDC_USD_PRICE * 10 ** (Constants.SYSTEM_DECIMALS - priceFeedDecimals));
+
+        // it should return the price
+        assertEq(expectedPrice.intoUint256(), price.intoUint256(), "price is not correct");
+    }
+}
