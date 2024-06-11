ec285acd021e6c88330d2088c98a458b353f5b5f
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2024-06-11 14:47:38 -0300

    refactor: name of sequencer uptime feed #368

--- script/utils/ProtocolConfiguration.sol
+++ script/utils/ProtocolConfiguration.sol
@@ -50,6 +50,6 @@ abstract contract ProtocolConfiguration is Markets, MarginCollaterals {
     uint256 internal constant MAX_MARGIN_REQUIREMENTS = 1e18;
     uint256 internal constant MOCK_DATA_STREAMS_EXPIRATION_DELAY = 5 seconds;
 
-    /// @notice Sequencer uptime feeds.
-    address internal constant SEQUENCER_UPTIME_FEEDS = 0xFdB631F5EE196F0ed6FAa767959853A9F217697D;
+    /// @notice Sequencer uptime feed.
+    address internal constant SEQUENCER_UPTIME_FEED = 0xFdB631F5EE196F0ed6FAa767959853A9F217697D;
 }
