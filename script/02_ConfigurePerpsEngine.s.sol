065c4d611e609fec9c27f7ca1c2b5828bc9869a9
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2024-06-12 16:14:23 -0300

    test: add scenario to check sequencer uptime feed on MarginCollateralConfiguration_GetPrice_Test #368

--- script/02_ConfigurePerpsEngine.s.sol
+++ script/02_ConfigurePerpsEngine.s.sol
@@ -41,7 +41,7 @@ contract ConfigurePerpsEngine is BaseScript, ProtocolConfiguration {
         tradingAccountToken.transferOwnership(address(perpsEngine));
 
         perpsEngine.setTradingAccountToken(address(tradingAccountToken));
-        perpsEngine.setSequencer(SEQUENCER_UPTIME_FEED);
+        perpsEngine.setSequencerUptimeFeed(SEQUENCER_UPTIME_FEED);
 
         configureSequencerUptimeFeeds(perpsEngine);
 
