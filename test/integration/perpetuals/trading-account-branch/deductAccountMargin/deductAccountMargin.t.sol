f3a893dc6572c676adc70347db8aea863a4a075b
Aspi <asparuhdamyanov@gmail.com>
2024-07-09 16:45:41 +0200

    add test

--- test/integration/perpetuals/trading-account-branch/deductAccountMargin/deductAccountMargin.t.sol
+++ test/integration/perpetuals/trading-account-branch/deductAccountMargin/deductAccountMargin.t.sol
@@ -62,6 +62,59 @@ contract DeductAccountMargin_Unit_Test is Base_Test, TradingAccountHarness {
         _;
     }
 
+    function test_GivenTheAccountHasAMarginBalanceDifferentFrom0(
+        uint256 marginValueUsd,
+        uint256 feeAmount,
+        uint256 randomFeeAmount1,
+        uint256 randomFeeAmount2
+    )
+        external
+        whenThereIsCollateralLiquidationPriority
+    {
+        // it should break the for loop
+
+        marginValueUsd = bound({ x: marginValueUsd, min: USDC_MIN_DEPOSIT_MARGIN, max: USDC_DEPOSIT_CAP });
+
+        feeAmount = bound({ x: feeAmount, min: USDC_MIN_DEPOSIT_MARGIN - 1, max: USDC_DEPOSIT_CAP - 1 });
+        randomFeeAmount1 = bound({ x: feeAmount, min: USDC_MIN_DEPOSIT_MARGIN - 1, max: USDC_DEPOSIT_CAP - 1 });
+        randomFeeAmount2 = bound({ x: feeAmount, min: USDC_MIN_DEPOSIT_MARGIN - 1, max: USDC_DEPOSIT_CAP - 1 });
+
+        deal({ token: address(usdc), to: users.naruto, give: marginValueUsd });
+
+        uint128 tradingAccountId = createAccountAndDeposit(marginValueUsd, address(usdc));
+
+        FillOrderContext memory ctx;
+
+        ctx.settlementFeeUsdX18 = ud60x18(randomFeeAmount1);
+
+        UD60x18 pnlUsdX18 = ud60x18(feeAmount);
+
+        UD60x18 orderFeeUsdX18 = ud60x18(randomFeeAmount2);
+
+        UD60x18 marginCollateralBalanceX18 = TradingAccountHarness(address(perpsEngine))
+            .exposed_getMarginCollateralBalance(tradingAccountId, address(usdc));
+
+        perpsEngine.exposed_withdrawMarginUsd(
+            tradingAccountId, address(usdc), ud60x18(marginValueUsd), marginCollateralBalanceX18, users.naruto
+        );
+
+        TradingAccountHarness(address(perpsEngine)).exposed_deductAccountMargin({
+            tradingAccountId: tradingAccountId,
+            feeRecipients: FeeRecipients.Data({
+                marginCollateralRecipient: MSIG_ADDRESS,
+                orderFeeRecipient: MSIG_ADDRESS,
+                settlementFeeRecipient: MSIG_ADDRESS
+            }),
+            pnlUsdX18: pnlUsdX18,
+            orderFeeUsdX18: orderFeeUsdX18,
+            settlementFeeUsdX18: ctx.settlementFeeUsdX18
+        });
+    }
+
+    modifier givenTheAccountHasAMarginBalanceOf0() {
+        _;
+    }
+
     function testFuzz_WhenTheSettlementFeeUsdX18IsZero(
         uint256 marginValueUsd,
         uint256 feeAmount,
@@ -69,6 +122,7 @@ contract DeductAccountMargin_Unit_Test is Base_Test, TradingAccountHarness {
         uint256 randomFeeAmount2
     )
         external
+        givenTheAccountHasAMarginBalanceOf0
         whenThereIsCollateralLiquidationPriority
     {
         // it should skip the settlementFeeUsdX18 check
@@ -110,6 +164,7 @@ contract DeductAccountMargin_Unit_Test is Base_Test, TradingAccountHarness {
         uint256 randomFeeAmount2
     )
         external
+        givenTheAccountHasAMarginBalanceOf0
         whenThereIsCollateralLiquidationPriority
     {
         // it should deduct the settlement fee from the account's margin balance
@@ -154,6 +209,7 @@ contract DeductAccountMargin_Unit_Test is Base_Test, TradingAccountHarness {
         uint256 randomFeeAmount2
     )
         external
+        givenTheAccountHasAMarginBalanceOf0
         whenThereIsCollateralLiquidationPriority
     {
         // it should skip the orderFeeUsdX18 check
@@ -196,6 +252,7 @@ contract DeductAccountMargin_Unit_Test is Base_Test, TradingAccountHarness {
         uint256 randomFeeAmount2
     )
         external
+        givenTheAccountHasAMarginBalanceOf0
         whenThereIsCollateralLiquidationPriority
     {
         // it should deduct the order fee from the account's margin balance
@@ -240,6 +297,7 @@ contract DeductAccountMargin_Unit_Test is Base_Test, TradingAccountHarness {
         uint256 randomFeeAmount2
     )
         external
+        givenTheAccountHasAMarginBalanceOf0
         whenThereIsCollateralLiquidationPriority
     {
         // it should skip the pnlUsdX18 check
@@ -282,6 +340,7 @@ contract DeductAccountMargin_Unit_Test is Base_Test, TradingAccountHarness {
         uint256 randomFeeAmount2
     )
         external
+        givenTheAccountHasAMarginBalanceOf0
         whenThereIsCollateralLiquidationPriority
     {
         // it should deduct the PnL from the account's margin balance
