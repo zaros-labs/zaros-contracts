7e1ff183aa5f7bb60ee9a94d7d078313efe1a5fc
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-07-15 11:50:36 -0300

    test: adds cancelAllOffchainOrders integration tests

--- test/integration/perpetuals/order-branch/cancelAllOffchainOrders/cancelAllOffchainOrders.t.sol
+++ test/integration/perpetuals/order-branch/cancelAllOffchainOrders/cancelAllOffchainOrders.t.sol
@@ -2,6 +2,9 @@
 pragma solidity 0.8.25;
 
 // Zaros dependencies
+import { Errors } from "@zaros/utils/Errors.sol";
+import { OrderBranch } from "@zaros/perpetuals/branches/OrderBranch.sol";
+import { TradingAccountHarness } from "test/harnesses/perpetuals/leaves/TradingAccountHarness.sol";
 import { Base_Test } from "test/Base.t.sol";
 
 contract CancelAllOffchainOrders_Integration_Test is Base_Test {
@@ -14,7 +17,15 @@ contract CancelAllOffchainOrders_Integration_Test is Base_Test {
     }
 
     function test_RevertGiven_TheTradingAccountDoesNotExist() external {
+        uint128 nonExistingTradingAccountId = 1;
+
         // it should revert
+        vm.expectRevert({
+            revertData: abi.encodeWithSelector(
+                Errors.AccountNotFound.selector, nonExistingTradingAccountId, users.naruto.account
+            )
+        });
+        perpsEngine.cancelAllOffchainOrders(nonExistingTradingAccountId);
     }
 
     modifier givenTheTradingAccountExists() {
@@ -22,11 +33,30 @@ contract CancelAllOffchainOrders_Integration_Test is Base_Test {
     }
 
     function test_RevertGiven_TheSenderIsNotTheOwner() external givenTheTradingAccountExists {
+        uint128 tradingAccountId = perpsEngine.createTradingAccount(bytes(""), false);
+
+        changePrank({ msgSender: users.sasuke.account });
         // it should revert
+        vm.expectRevert({
+            revertData: abi.encodeWithSelector(
+                Errors.AccountPermissionDenied.selector, tradingAccountId, users.sasuke.account
+            )
+        });
+        perpsEngine.cancelAllOffchainOrders(tradingAccountId);
     }
 
     function test_GivenTheSenderIsTheOwner() external givenTheTradingAccountExists {
-        // it should increase the trading account nonce
+        uint128 tradingAccountId = perpsEngine.createTradingAccount(bytes(""), false);
+        uint128 expectedNonce = 1;
+
         // it should emit {LogCancelAllOffchainOrders}
+        vm.expectEmit({ emitter: address(perpsEngine) });
+        emit OrderBranch.LogCancelAllOffchainOrders(users.naruto.account, tradingAccountId, expectedNonce);
+        perpsEngine.cancelAllOffchainOrders(tradingAccountId);
+
+        uint128 newNonce = TradingAccountHarness(address(perpsEngine)).workaround_getNonce(tradingAccountId);
+
+        // it should increase the trading account nonce
+        assertEq(expectedNonce, newNonce, "tradingAccount.nonce");
     }
 }
