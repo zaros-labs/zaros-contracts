2ea845ee29e297489d00564bf90287db0f1d8019
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-07-22 16:54:30 -0300

    chore: starts VaultBranch and Vault

--- src/market-making/branches/VaultBranch.sol
+++ src/market-making/branches/VaultBranch.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
-contract VaultBranch { }
+// Zaros dependencies
+import { Vault } from "@zaros/market-making/leaves/Vault.sol";
+
+contract VaultBranch {
+    function getVaultData(uint256 vaultId) external view returns (Vault.Data memory) {
+        return Vault.load(vaultId);
+    }
+
+    function deposit(uint128 vaultId, uint256 amount) external { }
+
+    // TODO: should we delay withdrawals?
+    function withdraw(uint128 vaultId, uint256 amount) external { }
+}
--- src/market-making/leaves/Vault.sol
+++ src/market-making/leaves/Vault.sol
@@ -1,4 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
-library Vault { }
+library Vault {
+    /// @notice ERC7201 storage location.
+    bytes32 internal constant VAULT_LOCATION =
+        keccak256(abi.encode(uint256(keccak256("fi.zaros.market-making.Vault")) - 1));
+
+    // TODO: pack storage slots
+    struct Data {
+        address vaultToken;
+        address collateralType;
+        uint256 vaultId;
+        uint256 totalDeposited;
+        int256 totalUnrealizedDebtUsd;
+        int256 totalRealizedDebtUsd;
+        bool isDegenVault;
+    }
+
+    /// @notice Loads a {Vault}.
+    /// @param vaultId The vault identifier.
+    /// @return vault The loaded vault storage pointer.
+    function load(uint256 vaultId) internal pure returns (Data storage vault) {
+        bytes32 slot = keccak256(abi.encode(VAULT_LOCATION, vaultId));
+        assembly {
+            vault.slot := slot
+        }
+    }
+}
