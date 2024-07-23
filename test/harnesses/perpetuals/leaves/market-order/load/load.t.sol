38f4be21292467c42ab0a8869c49c7bf38e62174
Aspi <asparuhdamyanov@gmail.com>
2024-07-23 15:47:39 +0300

    load existing unit tests

---
+++ test/harnesses/perpetuals/leaves/market-order/load/load.t.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+// Zaros dependencies
+import { Base_Test } from "test/Base.t.sol";
+import { MarketOrder } from "@zaros/perpetuals/leaves/MarketOrder.sol";
+
+contract Load_Unit_Test is Base_Test {
+    function setUp() public override {
+        Base_Test.setUp();
+        changePrank({ msgSender: users.owner.account });
+        configureSystemParameters();
+        createPerpMarkets();
+        changePrank({ msgSender: users.naruto.account });
+    }
+
+    function testFuzz_GivenYouHaveAMarketOrder(
+        uint256 initialMarginRate,
+        uint256 marginValueUsd,
+        uint256 marketId,
+        bool isLong
+    )
+        external
+    {
+        MarketConfig memory fuzzMarketConfig = getFuzzMarketConfig(marketId);
+
+        initialMarginRate = bound({ x: initialMarginRate, min: fuzzMarketConfig.imr, max: MAX_MARGIN_REQUIREMENTS });
+
+        marginValueUsd = bound({
+            x: marginValueUsd,
+            min: USDC_MIN_DEPOSIT_MARGIN,
+            max: convertUd60x18ToTokenAmount(address(usdc), USDC_DEPOSIT_CAP_X18)
+        });
+
+        deal({ token: address(usdc), to: users.naruto.account, give: marginValueUsd });
+
+        uint128 tradingAccountId = createAccountAndDeposit(marginValueUsd, address(usdc));
+
+        openPosition({
+            fuzzMarketConfig: fuzzMarketConfig,
+            tradingAccountId: tradingAccountId,
+            initialMarginRate: initialMarginRate,
+            marginValueUsd: marginValueUsd,
+            isLong: isLong
+        });
+
+        // it should load the market order
+
+        MarketOrder.Data memory marketOrder = perpsEngine.exposed_MarketOrder_load(tradingAccountId);
+    }
+
+    function testFuzz_GivenYouDoNotHaveAMarketOrder(uint128 tradingAccountId) external {
+        // it should not load the market order
+
+        MarketOrder.Data memory marketOrder = perpsEngine.exposed_MarketOrder_load(tradingAccountId);
+    }
+}
