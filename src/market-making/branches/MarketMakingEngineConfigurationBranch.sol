f644e3148c2fb9e61bf67a3f2e78d6ffd4850696
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-07-23 15:59:54 -0300

    chore: MarketMakingEngineConfigurationBranch functions v1

--- src/market-making/branches/MarketMakingEngineConfigurationBranch.sol
+++ src/market-making/branches/MarketMakingEngineConfigurationBranch.sol
@@ -1,4 +1,41 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
-contract MarketMakingEngineConfigurationBranch { }
+// Zaros dependencies
+import { MarketMakingEngineConfiguration } from "@zaros/market-making/leaves/MarketMakingEngineConfiguration.sol";
+
+// Open Zeppelin Upgradeable dependencies
+import { Initializable } from "@openzeppelin-upgradeable/proxy/utils/Initializable.sol";
+import { OwnableUpgradeable } from "@openzeppelin-upgradeable/access/OwnableUpgradeable.sol";
+
+contract MarketMakingEngineConfigurationBranch is Initializable, OwnableUpgradeable {
+    using MarketMakingEngineConfiguration for MarketMakingEngineConfiguration.Data;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    /// @dev The Ownable contract is initialized at the UpgradeBranch.
+    /// @dev {MarketMakingEngineConfigurationBranch} UUPS initializer.
+    function initialize(address usdToken, address perpsEngine) external initializer {
+        MarketMakingEngineConfiguration.Data storage marketMakingEngineConfiguration = MarketMakingEngine.load();
+
+        marketMakingEngineConfiguration.usdToken = usdToken;
+        marketMakingEngineConfiguration.perpsEngine = perpsEngine;
+    }
+
+    /// @notice Returns the address of custom referral code
+    /// @param customReferralCode The custom referral code.
+    /// @return referrer The address of the referrer.
+    function getCustomReferralCodeReferrer(string memory customReferralCode) external view returns (address) { }
+
+    function configureSystemParameters() external onlyOwner { }
+
+    function configureSequencerUptimeFeed() external onlyOwner { }
+
+    function createCustomReferralCode() external onlyOwner { }
+
+    function createVault() external onlyOwner { }
+
+    function updateVaultConfiguration() external onlyOwner { }
+}
