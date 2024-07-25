3f05bbce8efeb9821cf40a3b86eb1885f383845f
Aspi <asparuhdamyanov@gmail.com>
2024-07-25 13:51:55 +0300

    changing test location, adding assert

--- test/unit/perpetuals/market-order/load/load.t.sol
+++ test/unit/perpetuals/market-order/load/load.t.sol
@@ -4,6 +4,10 @@ pragma solidity 0.8.25;
 // Zaros dependencies
 import { Base_Test } from "test/Base.t.sol";
 import { MarketOrder } from "@zaros/perpetuals/leaves/MarketOrder.sol";
+import { SettlementConfiguration } from "@zaros/perpetuals/leaves/SettlementConfiguration.sol";
+
+// PRB Math dependencies
+import { ud60x18 } from "@prb-math/UD60x18.sol";
 
 contract Load_Unit_Test is Base_Test {
     function setUp() public override {
@@ -35,21 +39,32 @@ contract Load_Unit_Test is Base_Test {
         deal({ token: address(usdc), to: users.naruto.account, give: marginValueUsd });
 
         uint128 tradingAccountId = createAccountAndDeposit(marginValueUsd, address(usdc));
+        int128 sizeDelta = fuzzOrderSizeDelta(
+            FuzzOrderSizeDeltaParams({
+                tradingAccountId: tradingAccountId,
+                marketId: fuzzMarketConfig.marketId,
+                settlementConfigurationId: SettlementConfiguration.MARKET_ORDER_CONFIGURATION_ID,
+                initialMarginRate: ud60x18(initialMarginRate),
+                marginValueUsd: ud60x18(marginValueUsd),
+                maxSkew: ud60x18(fuzzMarketConfig.maxSkew),
+                minTradeSize: ud60x18(fuzzMarketConfig.minTradeSize),
+                price: ud60x18(fuzzMarketConfig.mockUsdPrice),
+                isLong: isLong,
+                shouldDiscountFees: true
+            })
+        );
 
-        openPosition({
-            fuzzMarketConfig: fuzzMarketConfig,
-            tradingAccountId: tradingAccountId,
-            initialMarginRate: initialMarginRate,
-            marginValueUsd: marginValueUsd,
-            isLong: isLong
-        });
+        perpsEngine.exposed_update(tradingAccountId, fuzzMarketConfig.marketId, sizeDelta);
 
         // it should load the market order
-        MarketOrder.Data memory marketOrder = perpsEngine.exposed_MarketOrder_load(tradingAccountId);
+        MarketOrder.Data memory marketOrder = perpsEngine.exposed_MarketOrder_loadExisting(tradingAccountId);
+
+        assertEq(marketOrder.marketId, fuzzMarketConfig.marketId);
     }
 
     function testFuzz_GivenYouDoNotHaveAMarketOrder(uint128 tradingAccountId) external {
         // it should not load the market order
         MarketOrder.Data memory marketOrder = perpsEngine.exposed_MarketOrder_load(tradingAccountId);
+        assertEq(marketOrder.marketId, 0);
     }
 }
