59414cb259623f523ff6929e72ec431d8c7b3d94
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-08-01 12:26:05 -0300

    feat: adds vault debt settlement priority queue

--- src/market-making/leaves/GlobalDebt.sol
+++ src/market-making/leaves/GlobalDebt.sol
@@ -11,11 +11,18 @@ library GlobalDebt {
     bytes32 internal constant GLOBAL_DEBT_LOCATION =
         keccak256(abi.encode(uint256(keccak256("fi.zaros.market-making.GlobalDebt")) - 1));
 
+    // TODO: work on encoding nodes into the priority queue
+    struct PriorityQueueNode {
+        uint128 vaultId;
+        int128 debt;
+    }
+
+    // TODO: pack storage slots
     struct Data {
         int256 totalUnsettledDebt;
         int256 totalSettledDebt;
         uint256 totalMarketsCreditWeight;
-        MinHeapLib.Heap vaultsDebtSettlementPriority;
+        MinHeapLib.Heap vaultsDebtSettlementPriorityQueue;
         Distribution.Data vaultsDebtDistribution;
     }
 
