207716ecca58764d7a1d35660cb332be2bd57150
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-08-06 17:04:43 -0300

    doc: add initial functions and document credit delegation branch

--- src/market-making/branches/CreditDelegationBranch.sol
+++ src/market-making/branches/CreditDelegationBranch.sol
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
-contract CreditDelegationBranch { }
+/// @dev This contract deals with USDC to settle protocol debt, used to back USDz
+contract CreditDelegationBranch {
+    function receiveMarginCollateral(address collateralType, uint256 amount) external { }
+
+    /// @dev Should settle vault's unsettled debt by converting the balance of different margin collateral types to
+    /// USDC, stored and used to cover future USDz swaps, and settle credit by converting the collected margin
+    /// collateral balance to the vaults' underlying assets.
+    /// @dev Settlement Priority:
+    /// 1. highest to lowest debt.
+    /// 2. highest to lowest credit.
+    /// @dev The protocol should also take into account the global debt state. E.g: if the protocol is in credit state
+    /// but a given vault is in net debt due to swaps, other vaults' exceeding credit (i.e exceeding assets) can be
+    /// converted to the in debt vault's underlying assets. If the protocol is in debt state but there's a vault with
+    /// net credit due to swaps, the protocol can rebalance other vaults by distributing exceeding assets from that
+    /// vault.
+    /// @dev In order to determine the logic above, it should be taken into account a vault's participation in the
+    /// global debt or credit. E.g if the protocol is in a given state and a new ZLP vault is added, this new vault is
+    /// neutral compared to the others that may be in credit or debt state.
+    function settleVaultsDebt() external { }
+
+    /// @dev Must be called whenever the perps trading engine needs to know a market's skew and OI caps.
+    /// @dev It takes into accounts all vault's credit delegated to each available markets. N Vaults may delegate
+    /// credit to N markets, configured by the protocol admin.
+    function updateCreditDelegation() external { }
+}
