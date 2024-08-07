e878048c36bf66832563580801235d7324986607
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-08-07 15:37:47 -0300

    chore: creates missing keepers

--- src/external/chainlink/keepers/usdz-swap-keeper/UsdzSwapKeeper.sol
+++ src/external/chainlink/keepers/usdz-swap-keeper/UsdzSwapKeeper.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
-// TODO: Make it a log automation keeper
+// TODO: Make it a log trigger, streams compatible, automation keeper
 contract UsdzSwapKeeper { }
