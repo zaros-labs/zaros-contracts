54fc61d717b166d806202e57acee04ded82ac07d
João Victor Pereira Santos <31141391+0xjoaovpsantos@users.noreply.github.com>
2024-08-09 14:46:01 -0300

    test: Create unit test for TradingAccount::create #507 (#509)
    * modules
    
    * test: Create unit test for TradingAccount::create #507
    
    * chore: .gitmodules #507

---
+++ test/unit/perpetuals/trading-account/create/create.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+// Zaros dependencies
+import { Base_Test } from "test/Base.t.sol";
+
+contract Create_Unit_Test is Base_Test {
+    function testFuzz_WhenTheCreateIsCalled(uint128 tradingAccountId, address owner) external {
+        vm.assume(owner != address(0));
+
+        changePrank({ msgSender: owner });
+
+        perpsEngine.exposed_create(tradingAccountId, owner);
+
+        // it should create the trading account
+        perpsEngine.exposed_loadExistingAccountAndVerifySender(tradingAccountId);
+
+        (uint128 tradingAccountIdReceived, address ownerReceived) =
+            perpsEngine.workaround_getTradingAccountIdAndOwner(tradingAccountId);
+
+        // it should save the trading account id
+        assertEq(tradingAccountId, tradingAccountIdReceived, "trading account id is not correct");
+
+        // it should save the owner
+        assertEq(owner, ownerReceived, "owner is not correct");
+    }
+}
