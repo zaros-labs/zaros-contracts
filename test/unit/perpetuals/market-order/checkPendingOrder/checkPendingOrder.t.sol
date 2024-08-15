013429f4ca440fc9219e47f54846a333c3164dc4
Asparuh Damyanov <asparuhdamyanov@gmail.com>
2024-08-15 17:34:52 +0300

    Test: checkPendingOrder unit tests (#516)
    * checkPendingOrder unit tests
    
    * test: testFuzz_WhenThereIsNoPendingOrder
    
    ---------
    
    Co-authored-by: Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>

---
+++ test/unit/perpetuals/market-order/checkPendingOrder/checkPendingOrder.t.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+// Zaros dependencies
+import { Base_Test } from "test/Base.t.sol";
+import { Errors } from "@zaros/utils/Errors.sol";
+import { SettlementConfiguration } from "@zaros/perpetuals/leaves/SettlementConfiguration.sol";
+
+// PRB Math dependencies
+import { ud60x18 } from "@prb-math/UD60x18.sol";
+
+contract CheckPendingOrder_Unit_Test is Base_Test {
+    function setUp() public override {
+        Base_Test.setUp();
+        changePrank({ msgSender: users.owner.account });
+        configureSystemParameters();
+        createPerpMarkets();
+        changePrank({ msgSender: users.naruto.account });
+    }
+
+    function testFuzz_RevertWhen_ThereIsAPendingOrder(
+        uint256 initialMarginRate,
+        uint256 marginValueUsd,
+        uint256 marketId,
+        bool isLong
+    )
+        external
+    {
+        MarketConfig memory fuzzMarketConfig = getFuzzMarketConfig(marketId);
+
+        initialMarginRate = bound({ x: initialMarginRate, min: fuzzMarketConfig.imr, max: MAX_MARGIN_REQUIREMENTS });
+
+        marginValueUsd = bound({
+            x: marginValueUsd,
+            min: USDC_MIN_DEPOSIT_MARGIN,
+            max: convertUd60x18ToTokenAmount(address(usdc), USDC_DEPOSIT_CAP_X18)
+        });
+
+        deal({ token: address(usdc), to: users.naruto.account, give: marginValueUsd });
+
+        uint128 tradingAccountId = createAccountAndDeposit(marginValueUsd, address(usdc));
+        int128 sizeDelta = fuzzOrderSizeDelta(
+            FuzzOrderSizeDeltaParams({
+                tradingAccountId: tradingAccountId,
+                marketId: fuzzMarketConfig.marketId,
+                settlementConfigurationId: SettlementConfiguration.MARKET_ORDER_CONFIGURATION_ID,
+                initialMarginRate: ud60x18(initialMarginRate),
+                marginValueUsd: ud60x18(marginValueUsd),
+                maxSkew: ud60x18(fuzzMarketConfig.maxSkew),
+                minTradeSize: ud60x18(fuzzMarketConfig.minTradeSize),
+                price: ud60x18(fuzzMarketConfig.mockUsdPrice),
+                isLong: isLong,
+                shouldDiscountFees: true
+            })
+        );
+
+        perpsEngine.exposed_update(tradingAccountId, fuzzMarketConfig.marketId, sizeDelta);
+
+        // it should revert
+        vm.expectRevert({
+            revertData: abi.encodeWithSelector(Errors.MarketOrderStillPending.selector, block.timestamp)
+        });
+        perpsEngine.exposed_checkPendingOrder(tradingAccountId);
+    }
+
+    function testFuzz_WhenThereIsNoPendingOrder(
+        uint256 initialMarginRate,
+        uint256 marginValueUsd,
+        uint256 marketId,
+        bool isLong
+    )
+        external
+    {
+        MarketConfig memory fuzzMarketConfig = getFuzzMarketConfig(marketId);
+
+        initialMarginRate = bound({ x: initialMarginRate, min: fuzzMarketConfig.imr, max: MAX_MARGIN_REQUIREMENTS });
+
+        marginValueUsd = bound({
+            x: marginValueUsd,
+            min: USDC_MIN_DEPOSIT_MARGIN,
+            max: convertUd60x18ToTokenAmount(address(usdc), USDC_DEPOSIT_CAP_X18)
+        });
+
+        deal({ token: address(usdc), to: users.naruto.account, give: marginValueUsd });
+
+        uint128 tradingAccountId = createAccountAndDeposit(marginValueUsd, address(usdc));
+
+        openPosition({
+            fuzzMarketConfig: fuzzMarketConfig,
+            tradingAccountId: tradingAccountId,
+            initialMarginRate: initialMarginRate,
+            marginValueUsd: marginValueUsd,
+            isLong: isLong
+        });
+
+        // it should continue execution
+        perpsEngine.exposed_checkPendingOrder(tradingAccountId);
+    }
+}
