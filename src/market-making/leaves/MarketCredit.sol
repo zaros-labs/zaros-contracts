4da2db750391a1e41be59dbcae218d19481ffc95
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-08-21 18:18:16 -0300

    chore: updates natspec

--- src/market-making/leaves/MarketCredit.sol
+++ src/market-making/leaves/MarketCredit.sol
@@ -16,13 +16,17 @@ library MarketCredit {
     // TODO: pack storage slots
     // TODO: add heap of in range and out range Vaults that provide credit to this market.
     /// @param marketId The perps engine's linked market id.
-    /// @param creditShare The market's share of the protocol total credit.
     /// @param autoDeleverageFactor A decimal rate which determines how much should the market cut of the position's
     /// positive pnl. Goes from 0 to 1.
     /// @param autoDeleverageThreshold An admin configurable decimal rate which determines when the market should
     /// enter the auto deleverage state. Goes from 0 to 1.
     /// @param autoDeleverageScale An admin configurable value which determines how much should the auto
     /// deleverage factor be.
+    /// @param settledDebtUsd The net delta of USDz minted by the market and margin collateral collected from traders.
+    /// @param inRangeVaults A heap of vaults that are actively delegating credit to this market.
+    /// @param outRangeVaults A heap of vaults that have stopped delegating credit to this market.
+    /// @param vaultsDebtDistribution `actor`: Vaults, `shares`: USD denominated credit delegated, `valuePerShare`:
+    /// USD denominated debt per share.
     struct Data {
         uint256 marketId;
         uint256 autoDeleverageThreshold;
@@ -35,11 +39,10 @@ library MarketCredit {
     }
 
     /// @notice Loads a {MarketCredit} namespace.
-    /// @param vaultId The id of a vault that provides credit to the market.
     /// @param marketId The perp market id.
     /// @return marketCredit The loaded market credit storage pointer.
-    function load(uint256 vaultId, uint256 marketId) internal pure returns (Data storage marketCredit) {
-        bytes32 slot = keccak256(abi.encode(MARKET_CREDIT_LOCATION, vaultId, marketId));
+    function load(uint256 marketId) internal pure returns (Data storage marketCredit) {
+        bytes32 slot = keccak256(abi.encode(MARKET_CREDIT_LOCATION, marketId));
         assembly {
             marketCredit.slot := slot
         }
