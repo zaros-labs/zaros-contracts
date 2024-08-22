da6f3d4fa9952bc236a6b03c235b8a89b1552ebf
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-08-22 17:14:06 -0300

    feat: getCreditForMarketId

--- src/market-making/leaves/MarketCredit.sol
+++ src/market-making/leaves/MarketCredit.sol
@@ -5,13 +5,15 @@ pragma solidity 0.8.25;
 import { Distribution } from "./Distribution.sol";
 
 // PRB Math dependencies
-import { UD60x18 } from "@prb-math/UD60x18.sol";
+import { UD60x18, ud60x18 } from "@prb-math/UD60x18.sol";
 
 // Solady dependencies
 import { MinHeapLib } from "@solady/Milady.sol";
 
 /// @dev A perp market won't be abl
 library MarketCredit {
+    using Distribution for Distribution.Data;
+
     /// @notice ERC7201 storage location.
     bytes32 internal constant MARKET_CREDIT_LOCATION =
         keccak256(abi.encode(uint256(keccak256("fi.zaros.market-making.MarketCredit")) - 1));
@@ -57,7 +59,19 @@ library MarketCredit {
 
     /// @notice Computes the auto delevarage factor of the market according to its debt state and configured
     /// parameters.
+    /// @param self The market credit storage pointer.
     /// @return autoDeleverageFactor A decimal rate which determines how much should the market cut of the position's
     /// positive pnl. Goes from 0 to 1.
     function getAutoDeleverageFactor(Data storage self) internal view returns (UD60x18 autoDeleverageFactor) { }
+
+    function getMarketCaps(Data storage self)
+        internal
+        view
+        returns (UD60x18 openInterestCapX18, UD60x18 skewCapX18)
+    {
+        UD60x18 totalDelegatedCredit = ud60x18(self.vaultsDebtDistribution.totalShares);
+
+        openInterestCapX18 = ud60x18(self.openInterestCapScale).mul(totalDelegatedCredit);
+        skewCapX18 = ud60x18(self.skewCapScale).mul(totalDelegatedCredit);
+    }
 }
