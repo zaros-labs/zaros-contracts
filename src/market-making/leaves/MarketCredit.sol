c03405e78e3e12a0d26622c534089678237c09b2
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2024-08-22 13:44:01 -0300

    chore: add oi and skew cap scales

--- src/market-making/leaves/MarketCredit.sol
+++ src/market-making/leaves/MarketCredit.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.25;
 // Zaros dependencies
 import { Distribution } from "./Distribution.sol";
 
+// PRB Math dependencies
+import { UD60x18 } from "@prb-math/UD60x18.sol";
+
 // Solady dependencies
 import { MinHeapLib } from "@solady/Milady.sol";
 
@@ -16,13 +19,16 @@ library MarketCredit {
     // TODO: pack storage slots
     // TODO: add heap of in range and out range Vaults that provide credit to this market.
     /// @param marketId The perps engine's linked market id.
-    /// @param autoDeleverageFactor A decimal rate which determines how much should the market cut of the position's
-    /// positive pnl. Goes from 0 to 1.
     /// @param autoDeleverageThreshold An admin configurable decimal rate which determines when the market should
     /// enter the auto deleverage state. Goes from 0 to 1.
     /// @param autoDeleverageScale An admin configurable value which determines how much should the auto
     /// deleverage factor be.
-    /// @param settledDebtUsd The net delta of USDz minted by the market and margin collateral collected from traders.
+    /// @param openInterestCapScale An admin configurable value which determines the market's open interest cap,
+    /// according to the total delegated credit.
+    /// @param skewCapScale An admin configurable value which determines the market's skew cap, according to the total
+    /// delegated credit.
+    /// @param realizedDebtUsd The net delta of USDz minted by the market and margin collateral collected from
+    /// traders.
     /// @param inRangeVaults A heap of vaults that are actively delegating credit to this market.
     /// @param outRangeVaults A heap of vaults that have stopped delegating credit to this market.
     /// @param vaultsDebtDistribution `actor`: Vaults, `shares`: USD denominated credit delegated, `valuePerShare`:
@@ -30,9 +36,10 @@ library MarketCredit {
     struct Data {
         uint256 marketId;
         uint256 autoDeleverageThreshold;
-        uint256 autoDeleverageFactor;
         uint256 autoDeleverageScale;
-        int256 settledDebtUsd;
+        uint256 openInterestCapScale;
+        uint256 skewCapScale;
+        int256 realizedDebtUsd;
         MinHeapLib.Heap inRangeVaults;
         MinHeapLib.Heap outRangeVaults;
         Distribution.Data vaultsDebtDistribution;
@@ -47,4 +54,10 @@ library MarketCredit {
             marketCredit.slot := slot
         }
     }
+
+    /// @notice Computes the auto delevarage factor of the market according to its debt state and configured
+    /// parameters.
+    /// @return autoDeleverageFactor A decimal rate which determines how much should the market cut of the position's
+    /// positive pnl. Goes from 0 to 1.
+    function getAutoDeleverageFactor(Data storage self) internal view returns (UD60x18 autoDeleverageFactor) { }
 }
