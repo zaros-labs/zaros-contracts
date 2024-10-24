128a10da882ad8dc9da0009b341e5017e2726bf1
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2024-10-24 14:25:53 -0300

    tests: FeeDistributionBranch::claimFees WIP

--- test/integration/market-making/fee-distribution-branch/claimFees/claimFees.t.sol
+++ test/integration/market-making/fee-distribution-branch/claimFees/claimFees.t.sol
@@ -13,6 +13,7 @@ contract ClaimFees_Integration_Test is Base_Test {
     function setUp() public virtual override {
         Base_Test.setUp();
         changePrank({ msgSender: address(users.owner.account) });
+        createVaults(marketMakingEngine, INITIAL_VAULT_ID, FINAL_VAULT_ID);
         configureMarkets();
     }
 
@@ -41,7 +42,41 @@ contract ClaimFees_Integration_Test is Base_Test {
         // it should revert
     }
 
-    function testFuzz_WhenAmountToClaimIsGreaterThenZero() external whenTheUserDoesHaveAvailableShares {
+    // TODO: Add the rest of the test cases
+    function testFuzz_WhenAmountToClaimIsGreaterThenZero(uint256 vaultId, uint256 marketId,
+        uint256 amountToDepositMarketFee, uint256 assetsToDepositVault) external whenTheUserDoesHaveAvailableShares {
+
+        changePrank({ msgSender: address(perpsEngine) });
+
+        uint128 uniswapV3StrategyId = 1;
+
+        PerpMarketCreditConfig memory fuzzPerpMarketCreditConfig = getFuzzPerpMarketCreditConfig(marketId);
+
+        amountToDepositMarketFee = bound({
+            x: amountToDepositMarketFee,
+            min: USDC_MIN_DEPOSIT_MARGIN,
+            max: convertUd60x18ToTokenAmount(address(usdc), USDC_DEPOSIT_CAP_X18)
+        });
+
+        deal({ token: address(usdc), to: address(perpsEngine), give: amountToDepositMarketFee });
+
+        marketMakingEngine.receiveMarketFee(fuzzPerpMarketCreditConfig.marketId, address(usdc), amountToDepositMarketFee);
+
+        marketMakingEngine.convertAccumulatedFeesToWeth(
+            fuzzPerpMarketCreditConfig.marketId, address(usdc), uniswapV3StrategyId
+        );
+
+        changePrank({ msgSender: users.naruto.account });
+
+        VaultConfig memory fuzzVaultConfig = getFuzzVaultConfig(vaultId);
+
+        assetsToDepositVault = bound({ x: assetsToDepositVault, min: 1, max: fuzzVaultConfig.depositCap });
+        deal(fuzzVaultConfig.asset, users.naruto.account, assetsToDepositVault);
+
+        marketMakingEngine.deposit(fuzzVaultConfig.vaultId, uint128(assetsToDepositVault), 0);
+
+        marketMakingEngine.claimFees(fuzzVaultConfig.vaultId);
+
         // it should update accumulate actor
         // it should transfer the fees to the sender
         // it should emit {LogClaimFees} event
