03180a237e23ba2c893500afb66383e5d3c4a9e6
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2024-11-15 03:03:07 -0300

    test: wip CreditDelegationBranch::getCreditCapacityForMarketId

--- test/integration/market-making/credit-delegation-branch/getCreditCapacityForMarketId.t.sol
+++ test/integration/market-making/credit-delegation-branch/getCreditCapacityForMarketId.t.sol
@@ -4,19 +4,8 @@ pragma solidity 0.8.25;
 // Zaros dependencies
 import { Base_Test } from "test/Base.t.sol";
 import { Errors } from "@zaros/utils/Errors.sol";
-import { FeeDistributionBranch } from "@zaros/market-making/branches/FeeDistributionBranch.sol";
-import { UniswapV3Adapter } from "@zaros/utils/dex-adapters/UniswapV3Adapter.sol";
-import { Math } from "@zaros/utils/Math.sol";
-
-// Openzeppelin dependencies
-import { EnumerableSet } from "@openzeppelin/utils/structs/EnumerableSet.sol";
-import { IERC20 } from "@openzeppelin/token/ERC20/extensions/ERC4626.sol";
-
-// PRB Math dependencies
-import { UD60x18, ud60x18 } from "@prb-math/UD60x18.sol";
 
 contract CreditDelegationBranch_GetCreditCapacityForMarketId_Integration_Test is Base_Test {
-    using EnumerableSet for EnumerableSet.UintSet;
 
     function setUp() public virtual override {
         Base_Test.setUp();
