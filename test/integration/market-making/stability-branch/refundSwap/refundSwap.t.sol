c7fe85e6627c0f6648f34ac97eb41c008e86eb42
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2024-12-16 12:37:45 -0300

    fix: RefundSwap_Integration_Test::testFuzz_WhenDeadlineHasPassed

--- test/integration/market-making/stability-branch/refundSwap/refundSwap.t.sol
+++ test/integration/market-making/stability-branch/refundSwap/refundSwap.t.sol
@@ -98,9 +98,13 @@ contract RefundSwap_Integration_Test is Base_Test {
     function testFuzz_WhenDeadlineHasPassed(uint256 vaultId, uint256 swapAmount) external whenRequestIsNotProcessed {
         VaultConfig memory fuzzVaultConfig = getFuzzVaultConfig(vaultId);
 
-        deal({ token: address(fuzzVaultConfig.asset), to: fuzzVaultConfig.indexToken, give: type(uint256).max });
+        deal({ token: address(fuzzVaultConfig.asset), to: fuzzVaultConfig.indexToken, give: fuzzVaultConfig.depositCap });
 
-        swapAmount = bound({ x: swapAmount, min: 1e18, max: type(uint128).max });
+        UD60x18 assetPriceX18 = IPriceAdapter(fuzzVaultConfig.priceAdapter).getPrice();
+        UD60x18 assetAmountX18 = ud60x18(IERC4626(fuzzVaultConfig.indexToken).totalAssets());
+        uint256 maxSwapAmount = assetAmountX18.mul(assetPriceX18).intoUint256();
+
+        swapAmount = bound({ x: swapAmount, min: 1e18, max: maxSwapAmount });
 
         deal({ token: address(usdToken), to: users.naruto.account, give: swapAmount });
 
