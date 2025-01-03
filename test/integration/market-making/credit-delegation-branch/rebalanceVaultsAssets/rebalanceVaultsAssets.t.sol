b9ff2441f3d34cf5809e24a4e7d6be8c71cc666d
João Victor Pereira Santos <31141391+0xjoaovpsantos@users.noreply.github.com>
2025-01-03 09:00:04 -0300

    fix: breaking tests create vault function (#726)

--- test/integration/market-making/credit-delegation-branch/rebalanceVaultsAssets/rebalanceVaultsAssets.t.sol
+++ test/integration/market-making/credit-delegation-branch/rebalanceVaultsAssets/rebalanceVaultsAssets.t.sol
@@ -11,7 +11,7 @@ contract CreditDelegationBranch_RebalanceVaultsAssets_Integration_Test is Base_T
     function setUp() public virtual override {
         Base_Test.setUp();
         changePrank({ msgSender: users.owner.account });
-        createVaults(marketMakingEngine, INITIAL_VAULT_ID, FINAL_VAULT_ID);
+        createVaults(marketMakingEngine, INITIAL_VAULT_ID, FINAL_VAULT_ID, true, address(perpsEngine));
         configureMarkets();
     }
 
