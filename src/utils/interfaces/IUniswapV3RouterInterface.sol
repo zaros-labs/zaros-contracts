4b828db6f03dfa3a307cfa41cdbc65bada740b5b
João Victor Pereira Santos <31141391+0xjoaovpsantos@users.noreply.github.com>
2025-01-19 04:03:32 -0300

    fix: remove @audit tag about the engine in the Vault.sol #740 (#772)

--- src/utils/interfaces/IUniswapV3RouterInterface.sol
+++ src/utils/interfaces/IUniswapV3RouterInterface.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
 import { IUniswapV3SwapCallback } from "@zaros/utils/interfaces/IUniswapV3SwapCallback.sol";
